d14b83f506bdffd7ffb0e502ec1c47b3ff3d426a
Farheen Iqbal <iqbalfarheen@gmail.com>
2021-04-05 21:36:28 +0530

    added list of tasks page

--- app/common/types.ts
+++ app/common/types.ts
@@ -4,3 +4,8 @@ export interface Video {
   readonly title: string
   readonly duration: string
 }
+export interface TaskType {
+  readonly id: number
+  readonly title: string
+  readonly description: string
+}
