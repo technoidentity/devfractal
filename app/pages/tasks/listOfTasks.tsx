208d508a1f621f5ebf368bd533abdd8eb6a8336b
Farheen Iqbal <iqbalfarheen@gmail.com>
2021-04-06 16:57:47 +0530

    moved task related files in pages/tasks and pages/api/tasks folders

--- app/pages/tasks/listOfTasks.tsx
+++ app/pages/tasks/listOfTasks.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import useSWR from 'swr'
-import type { Task } from '../common'
-import { TaskList } from '../components/TaskList'
+import type { Task } from '../../common'
+import { TaskList } from '../../components/TaskList'
 
 const fetcher = async (url: string) => fetch(url).then(async res => res.json())
 
 const ListOfTasks = () => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { data, error } = useSWR('/api/tasks', fetcher)
+  const { data, error } = useSWR('/api/tasks/tasks', fetcher)
 
   if (error) {
     return <div>failed to load</div>
