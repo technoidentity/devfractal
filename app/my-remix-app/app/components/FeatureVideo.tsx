479f61a188d8536e8915db59fa61a5adab2d4572
sravanidande <sravanidnd89@gmail.com>
2022-12-23 13:28:58 +0530

    add Feature video for dashboard

---
+++ app/my-remix-app/app/components/FeatureVideo.tsx
@@ -0,0 +1,137 @@
+import React from 'react'
+import {
+  Box,
+  Heading,
+  Link,
+  Image,
+  Text,
+  Divider,
+  HStack,
+  Tag,
+  Wrap,
+  WrapItem,
+  SpaceProps,
+  useColorModeValue,
+  Container,
+  VStack,
+} from '@chakra-ui/react'
+
+interface IBlogTags {
+  tags: Array<string>
+  marginTop?: SpaceProps['marginTop']
+}
+
+const BlogTags: React.FC<IBlogTags> = props => {
+  return (
+    <HStack spacing={2} marginTop={props.marginTop}>
+      {props.tags.map(tag => {
+        return (
+          <Tag size={'md'} variant="solid" colorScheme="orange" key={tag}>
+            {tag}
+          </Tag>
+        )
+      })}
+    </HStack>
+  )
+}
+
+interface BlogAuthorProps {
+  date: Date
+  name: string
+}
+
+export const BlogAuthor: React.FC<BlogAuthorProps> = props => {
+  return (
+    <HStack marginTop="2" spacing="2" display="flex" alignItems="center">
+      <Image
+        borderRadius="full"
+        boxSize="40px"
+        src="https://100k-faces.glitch.me/random-image"
+        alt={`Avatar of ${props.name}`}
+      />
+      <Text fontWeight="medium">{props.name}</Text>
+      <Text>—</Text>
+      <Text>{props.date.toLocaleDateString()}</Text>
+    </HStack>
+  )
+}
+
+const FeatureVideo = () => {
+  return (
+    <Container maxW={'7xl'} p="12">
+      <Heading as="h1">Learn from here</Heading>
+      <Box
+        marginTop={{ base: '1', sm: '5' }}
+        display="flex"
+        flexDirection={{ base: 'column', sm: 'row' }}
+        justifyContent="space-between"
+      >
+        <Box
+          display="flex"
+          flex="1"
+          marginRight="3"
+          position="relative"
+          alignItems="center"
+        >
+          <Box
+            width={{ base: '100%', sm: '85%' }}
+            zIndex="2"
+            marginLeft={{ base: '0', sm: '5%' }}
+            marginTop="5%"
+          >
+            <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+              <Image
+                borderRadius="lg"
+                src={
+                  'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
+                }
+                alt="some good alt text"
+                objectFit="contain"
+              />
+            </Link>
+          </Box>
+          <Box zIndex="1" width="100%" position="absolute" height="100%">
+            <Box
+              bgGradient={useColorModeValue(
+                'radial(orange.600 1px, transparent 1px)',
+                'radial(orange.300 1px, transparent 1px)',
+              )}
+              backgroundSize="20px 20px"
+              opacity="0.4"
+              height="100%"
+            />
+          </Box>
+        </Box>
+        <Box
+          display="flex"
+          flex="1"
+          flexDirection="column"
+          justifyContent="center"
+          marginTop={{ base: '3', sm: '0' }}
+        >
+          <BlogTags tags={['React', 'Javascript']} />
+          <Heading marginTop="1">
+            <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+              Reactive Programming
+            </Link>
+          </Heading>
+          <Text
+            as="p"
+            marginTop="2"
+            color={useColorModeValue('gray.700', 'gray.200')}
+            fontSize="lg"
+          >
+            Reactive programming describes a design paradigm that relies on
+            asynchronous programming logic to handle real-time updates to
+            otherwise static content. It provides an efficient means -- the use
+            of automated data streams -- to handle data updates to content
+            whenever a user makes an inquiry.
+          </Text>
+          <BlogAuthor name="John Doe" date={new Date('2021-04-06T19:01:27Z')} />
+        </Box>
+      </Box>
+    </Container>
+  )
+}
+
+export default FeatureVideo
