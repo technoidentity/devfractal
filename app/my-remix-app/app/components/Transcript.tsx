8d362ed600011d63f03799d05164d1c763a9d263
sravanidande <sravanidnd89@gmail.com>
2022-12-23 13:27:57 +0530

    add Transcript for specific video

---
+++ app/my-remix-app/app/components/Transcript.tsx
@@ -0,0 +1,97 @@
+import {
+  Container,
+  Divider,
+  Heading,
+  HStack,
+  Image,
+  SpaceProps,
+  Tag,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
+import React from 'react'
+
+interface IBlogTags {
+  tags: Array<string>
+  marginTop?: SpaceProps['marginTop']
+}
+
+const BlogTags: React.FC<IBlogTags> = props => {
+  return (
+    <HStack spacing={2} marginTop={props.marginTop}>
+      {props.tags.map(tag => {
+        return (
+          <Tag size={'md'} variant="solid" colorScheme="orange" key={tag}>
+            {tag}
+          </Tag>
+        )
+      })}
+    </HStack>
+  )
+}
+
+interface BlogAuthorProps {
+  date: Date
+  name: string
+}
+
+export const BlogAuthor: React.FC<BlogAuthorProps> = props => {
+  return (
+    <HStack marginTop="2" spacing="2" display="flex" alignItems="center">
+      <Image
+        borderRadius="full"
+        boxSize="40px"
+        src="https://100k-faces.glitch.me/random-image"
+        alt={`Avatar of ${props.name}`}
+      />
+      <Text fontWeight="medium">{props.name}</Text>
+      <Text>—</Text>
+      <Text>{props.date.toLocaleDateString()}</Text>
+    </HStack>
+  )
+}
+
+const Transcript = () => {
+  return (
+    <Container maxW={'7xl'}>
+      <Divider marginTop="5" />
+
+      <VStack
+        paddingTop="40px"
+        paddingLeft="40px"
+        paddingRight="40px"
+        spacing="2"
+        mb="60px"
+        alignItems="flex-start"
+      >
+        <Heading as="h2">What we write about</Heading>
+        <Text as="p" fontSize="lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          condimentum quam arcu, eu tempus tortor molestie at. Vestibulum
+          pretium condimentum dignissim. Vestibulum ultrices vitae nisi sed
+          imperdiet. Mauris quis erat consequat, commodo massa quis, feugiat
+          sapien. Suspendisse placerat vulputate posuere. Curabitur neque
+          tortor, mattis nec lacus non, placerat congue elit.
+        </Text>
+        <Text as="p" fontSize="lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          condimentum quam arcu, eu tempus tortor molestie at. Vestibulum
+          pretium condimentum dignissim. Vestibulum ultrices vitae nisi sed
+          imperdiet. Mauris quis erat consequat, commodo massa quis, feugiat
+          sapien. Suspendisse placerat vulputate posuere. Curabitur neque
+          tortor, mattis nec lacus non, placerat congue elit.
+        </Text>
+        <Text as="p" fontSize="lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          condimentum quam arcu, eu tempus tortor molestie at. Vestibulum
+          pretium condimentum dignissim. Vestibulum ultrices vitae nisi sed
+          imperdiet. Mauris quis erat consequat, commodo massa quis, feugiat
+          sapien. Suspendisse placerat vulputate posuere. Curabitur neque
+          tortor, mattis nec lacus non, placerat congue elit.
+        </Text>
+      </VStack>
+    </Container>
+  )
+}
+
+export default Transcript
