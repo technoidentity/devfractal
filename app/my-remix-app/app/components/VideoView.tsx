e14de01cbd3605d0be90bded582f694244e17924
sravanidande <sravanidnd89@gmail.com>
2022-12-24 22:44:52 +0530

    fix issue with iframe videoUrl

--- app/my-remix-app/app/components/VideoView.tsx
+++ app/my-remix-app/app/components/VideoView.tsx
@@ -6,14 +6,17 @@ interface VideoProps {
 }
 
 export const VideoView = ({ video }: VideoProps) => {
+  const videoUrl = video.url.split('=')
   return (
     <Flex flex={2} p="10px" w="100%" minH="50vh">
       <Stack h="100%" w="100%">
         <iframe
           title={video.title}
-          src={`https://www.youtube.com/embed/${video.url}`}
+          src={`https://www.youtube.com/embed/${videoUrl[1]}`}
           height="100%"
           width="100%"
+          frameBorder="0"
+          allowFullScreen
         />
       </Stack>
     </Flex>
