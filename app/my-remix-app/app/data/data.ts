2b0c31744b9ae5f6d087d6e866f7c998691f4b1c
sravanidande <sravanidnd89@gmail.com>
2022-12-24 22:44:17 +0530

    seed database with real time data

---
+++ app/my-remix-app/app/data/data.ts
@@ -0,0 +1,44 @@
+import { Course, Video } from '@prisma/client'
+import { v4 as uuid } from 'uuid'
+
+export const reactCourse: Course = {
+  id: uuid(),
+  title: 'React fundamentals',
+  description:
+    'This course is for React newbies and anyone looking to build a solid foundation. It’s designed to teach you everything you need to start building web applications in React right away.',
+}
+
+export const reactVideos: Video[] = [
+  {
+    id: uuid(),
+    courseId: reactCourse.id,
+    url: 'https://www.youtube.com/watch?v=s2skans2dP4',
+    title: "Beginner's guide to React",
+    description:
+      'A list of the core concepts every React developer should have a solid grasp on.',
+  },
+  {
+    id: uuid(),
+    courseId: reactCourse.id,
+    url: 'https://www.youtube.com/watch?v=0jlTw2XI7I8',
+    title: 'React Hooks',
+    description:
+      'Hooks are functions that let you “hook into” React state and lifecycle features from function components.',
+  },
+  {
+    id: uuid(),
+    courseId: reactCourse.id,
+    url: 'https://www.youtube.com/watch?v=zpUMRsAO6-Y',
+    title: 'State Management in React',
+    description:
+      'As your application grows, it helps to be more intentional about how your state is organized and how the data flows between your components.',
+  },
+  {
+    id: uuid(),
+    courseId: reactCourse.id,
+    url: 'https://www.youtube.com/watch?v=AiJ8tRRH0f8',
+    title: 'Simply React',
+    description:
+      'Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.',
+  },
+]
--- app/my-remix-app/prisma/seed.ts
+++ app/my-remix-app/prisma/seed.ts
@@ -3,45 +3,44 @@ import { PrismaClient, User } from '@prisma/client'
 import { range } from 'lodash'
 import { db } from '~/utils/db.server'
 const prisma = new PrismaClient()
+import { courseList, reactCourse, reactVideos } from '~/data/data'
 
-function createVideos(n: number) {
-  return range(1, n).map(_ => ({
-    title: faker.name.fullName(),
-    description: faker.lorem.sentences(2),
-    url: 'xGCm_cLxets',
-  }))
-}
+// function createVideos(n: number) {
+//   return range(1, n).map(_ => ({
+//     title: faker.name.fullName(),
+//     description: faker.lorem.sentences(2),
+//     url: 'xGCm_cLxets',
+//   }))
+// }
 
-function createUsers(n: number): Pick<User, 'username' | 'passwordHash'>[] {
-  return range(1, n).map(_ => ({
-    username: faker.name.firstName(),
-    passwordHash: 'secret',
-  }))
+// const courses = [{}]
+
+// function createUsers(n: number): Pick<User, 'username' | 'passwordHash'>[] {
+//   return range(1, n).map(_ => ({
+//     username: faker.name.firstName(),
+//     passwordHash: 'secret',
+//   }))
+// }
+
+const createReactVideos = async () => {
+  const course = await prisma.course.create({
+    data: reactCourse,
+  })
+
+  const videos = await reactVideos.map(
+    async v =>
+      await prisma.video.create({
+        data: { ...v, courseId: course.id },
+      }),
+  )
+  return {
+    course,
+    videos,
+  }
 }
 
 async function main() {
-  for (let i = 0; i <= 9; i += 1) {
-    await prisma.course.create({
-      data: {
-        title: faker.name.fullName(),
-        description: faker.lorem.sentence(),
-        videos: {
-          create: createVideos(3),
-        },
-      },
-    })
-
-    await Promise.all(
-      createUsers(6).map(user => {
-        return db.user.create({
-          data: {
-            username: user.username,
-            passwordHash: user.passwordHash,
-          },
-        })
-      }),
-    )
-  }
+  await createReactVideos()
 }
 
 main()
@@ -52,3 +51,26 @@ main()
   .finally(async () => {
     await prisma.$disconnect()
   })
+
+//  for (let i = 0; i <= 9; i += 1) {
+//    await prisma.course.create({
+//      data: {
+//        title: faker.name.fullName(),
+//        description: faker.lorem.sentence(),
+//        videos: {
+//          create: createVideos(3),
+//        },
+//      },
+//    })
+
+//    await Promise.all(
+//      createUsers(6).map(user => {
+//        return db.user.create({
+//          data: {
+//            username: user.username,
+//            passwordHash: user.passwordHash,
+//          },
+//        })
+//      }),
+//    )
+//  }
