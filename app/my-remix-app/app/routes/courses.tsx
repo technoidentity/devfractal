d025fddeeb1199e4fe1363a5666b9af0ec547011
sravanidande <sravanidnd89@gmail.com>
2022-12-24 22:43:39 +0530

    handle unAuthroized user

--- app/my-remix-app/app/routes/courses.tsx
+++ app/my-remix-app/app/routes/courses.tsx
@@ -9,25 +9,29 @@ import {
   getUserSession,
   requireUserId,
 } from '~/services/session.server'
+import Unauthorized from '~/components/UnAuthorized'
 
 type LoaderData = {
   courses: Course[]
+  userId: string | null
 }
 
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await getUserId(request)
   console.log(userId)
   if (!userId) {
-    return redirect('/')
+    return null
+  } else {
+    const courses = await db.course.findMany()
+    return json<LoaderData>({ courses, userId })
   }
-  const courses = await db.course.findMany()
-  return json<LoaderData>({ courses })
 }
 
 const CourseList = () => {
-  const { courses } = useLoaderData<LoaderData>()
+  const data = useLoaderData<LoaderData>()
+  console.log('unauth')
 
-  return <Dashboard courses={courses} />
+  return data !== null ? <Dashboard courses={data.courses} /> : <Unauthorized />
 }
 
 export default CourseList
