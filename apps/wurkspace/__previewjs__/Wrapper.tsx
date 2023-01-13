f93344bf2599fe782ce0a7910c42e603f05eea88
Pervez Iqbal <pervezfunctor@gmail.com>
2023-01-13 16:12:27 +0530

    previewjs for all next projects

--- apps/wurkspace/__previewjs__/Wrapper.tsx
+++ apps/wurkspace/__previewjs__/Wrapper.tsx
@@ -1,6 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
-import '@previewjs/config-helper-nextjs'
 
 export const Wrapper = ({ children }: any) => (
   <ChakraProvider>{children}</ChakraProvider>
