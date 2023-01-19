d715b801ca0d16fb0713b653b1d865ea605263f1
Pervez Iqbal <pervezfunctor@gmail.com>
2023-01-19 14:09:44 +0530

    temporary fix for previewjs for reducer-examples

--- apps/reducer-examples/__previewjs__/Wrapper.tsx
+++ apps/reducer-examples/__previewjs__/Wrapper.tsx
@@ -1,6 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import '../styles/global.css'
-import '@previewjs/config-helper-nextjs'
+// import '@previewjs/config-helper-nextjs'
 
 export const Wrapper = ({ children }: any) => (
   <ChakraProvider>{children}</ChakraProvider>
