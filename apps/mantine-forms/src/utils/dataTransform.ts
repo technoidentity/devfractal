623fab71758714b4f234446484191d7c45773aae
sravanidande <sravanidnd89@gmail.com>
2023-01-24 21:10:18 +0530

    add types and date transforms

---
+++ apps/mantine-forms/src/utils/dataTransform.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Products } from './types'
+
+export const categorizedProducts = (data: Products) => {
+  // return data.reduce((acc, v) => {
+  //   if (!acc[v.category]) {
+  //     acc[v.category] = []
+  //   }
+  //   acc[v.category].push(v)
+  //   return acc
+  // }, {})
+
+  const result: any = {}
+  for (const obj of data) {
+    if (!result[obj.category]) {
+      result[obj.category] = [obj]
+    } else {
+      result[obj.category].push(obj)
+    }
+  }
+  return result as Record<string, Products>
+}
---
+++ apps/mantine-forms/src/utils/types.ts
@@ -0,0 +1,41 @@
+/* eslint-disable no-redeclare */
+import { z } from 'zod'
+
+export const Product = z.object({
+  category: z.string(),
+  price: z.string(),
+  stocked: z.boolean(),
+  name: z.string(),
+})
+
+export type Product = z.infer<typeof Product>
+
+export const Products = z.array(Product)
+
+export type Products = z.infer<typeof Products>
+
+export const Person = z.object({
+  id: z.number(),
+  name: z.string(),
+  imageId: z.string(),
+})
+
+export type Person = z.infer<typeof Person>
+
+export const People = z.array(Person)
+
+export type People = z.infer<typeof People>
+
+export const Sculpture = z.object({
+  name: z.string(),
+  artist: z.string(),
+  description: z.string(),
+  url: z.string(),
+  alt: z.string(),
+})
+
+export type Sculpture = z.infer<typeof Sculpture>
+
+export const SculptureList = z.array(Sculpture)
+
+export type SculptureList = z.infer<typeof SculptureList>
