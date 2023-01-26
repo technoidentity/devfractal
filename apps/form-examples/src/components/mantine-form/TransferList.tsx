72a5a2ea6864b33bca85dd2d4c51016b7fcb82c3
sravanidande <sravanidnd89@gmail.com>
2023-01-26 21:20:31 +0530

    add TransferList component

---
+++ apps/form-examples/src/components/mantine-form/TransferList.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { TransferList, TransferListData } from '@mantine/core'
+
+const initialValues: TransferListData = [
+  [
+    { value: 'react', label: 'React' },
+    { value: 'ng', label: 'Angular' },
+    { value: 'next', label: 'Next.js' },
+    { value: 'blitz', label: 'Blitz.js' },
+    { value: 'gatsby', label: 'Gatsby.js' },
+    { value: 'vue', label: 'Vue' },
+    { value: 'jq', label: 'jQuery' },
+  ],
+  [
+    { value: 'sv', label: 'Svelte' },
+    { value: 'rw', label: 'Redwood' },
+    { value: 'np', label: 'NumPy' },
+    { value: 'dj', label: 'Django' },
+    { value: 'fl', label: 'Flask' },
+  ],
+]
+
+export const TransferListExample = () => {
+  const [data, setData] = useState<TransferListData>(initialValues)
+  return (
+    <TransferList
+      value={data}
+      onChange={setData}
+      searchPlaceholder="Search..."
+      nothingFound="Nothing here"
+      titles={['Frameworks', 'Libraries']}
+      breakpoint="sm"
+    />
+  )
+}
