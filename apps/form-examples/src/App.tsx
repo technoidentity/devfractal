bf2d0900397b77af18424e3f31ffe671131d9083
Pervez Iqbal <pervezfunctor@gmail.com>
2023-01-27 13:57:35 +0530

    jotai cart to global-state-examples

--- apps/form-examples/src/App.tsx
+++ apps/form-examples/src/App.tsx
@@ -1,8 +1,10 @@
 import { MantineProvider } from '@mantine/core'
-import { MantineForm } from './components/mantine-form/MantineForm'
+// import { Accordian } from './components/caurosel/accordion/Accordion'
+// import { CartApp } from './components/e-cart/CartApp'
+// import { MantineForm } from './components/mantine-form/MantineForm'
 // import { StepperForm } from './components/stepper-form/StepperForm'
 // import { Carousel } from './components/caurosel/caurosel/Carousel'
-// import { CartApp } from './components/e-cart/CartApp'
+import { CartApp } from './components/e-cart/CartApp'
 // import { TicTacToe } from './components/tic-tac-toe/TicTacToe'
 // import { StepperForm } from './components/stepper-form/StepperForm'
 // import { sculptureList } from './utils/data'
@@ -13,13 +15,14 @@ export function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
       {/* <Text>Hello</Text> */}
-
+      {/* <Accordian /> */}
+      <CartApp />
       {/* <Text>Welcome to Mantine!</Text> */}
       {/* <FilterableProductTable /> */}
       {/* <Carousel sculptures={sculptureList} /> */}
       {/* <StepperForm /> */}
       {/* <TicTacToe /> */}
-      <MantineForm />
+      {/* <MantineForm /> */}
     </MantineProvider>
   )
 }
