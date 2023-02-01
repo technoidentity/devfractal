fbd10e078f46c74e251cb879fe3dc24f91bd1ca2
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-01 21:43:56 +0530

    useTable WIP

--- libs/table/src/useTable.tsx
+++ libs/table/src/useTable.tsx
@@ -1,77 +1,45 @@
-// import { castDraft, Draft } from 'immer'
-// import { uniqBy } from 'lodash-es'
-// import React = require('react')
+import { state } from '@srtp/local-state'
+import { castDraft } from 'immer'
+import { uniqBy } from 'lodash-es'
 
-// export interface CrudTableState<T> {
-//   readonly sort: keyof T
-//   readonly order: 'asc' | 'desc'
+export type CrudTableState<T extends object> = Readonly<{
+  sort: keyof T
+  order: 'asc' | 'desc'
 
-//   readonly selectedRows: readonly T[]
-//   readonly selectedColumns: ReadonlyArray<keyof T>
+  selectedRows: readonly T[]
+  selectedColumns: ReadonlyArray<keyof T>
 
-//   readonly page: number
-//   // readonly perPage: number
-// }
+  page: number
+  // readonly perPage: number
+}>
 
-// export type CrudTableAction<T> =
-//   | { readonly type: 'change sort order'; readonly sort: keyof T }
-//   | {
-//       readonly type: 'select columns'
-//       readonly columns: ReadonlyArray<keyof T>
-//     }
-//   | { readonly type: 'toggle row'; readonly row: T; readonly idKey: keyof T }
-//   | { readonly type: 'change page'; readonly page: number }
-// // | { readonly type: 'change per page'; readonly perPage: number }
+export const useCrudTable = <T extends object>(
+  initialState: CrudTableState<T>,
+) =>
+  state(initialState, {
+    changeSortOrder(state, sort: keyof T) {
+      if (sort === state.sort) {
+        state.order = state.order === 'asc' ? 'desc' : 'asc'
+      } else {
+        state.sort = castDraft(sort)
+      }
+    },
+    selectColumns(state, columns: ReadonlyArray<keyof T>) {
+      state.selectedColumns = castDraft(columns)
+    },
+    changePage(state, page: number) {
+      state.page = page
+    },
+    toggleRow(state, { row, idKey }: Readonly<{ row: T; idKey: keyof T }>) {
+      const idx = state.selectedRows.findIndex(
+        r => (r as T)[idKey] === row[idKey],
+      )
 
-// export function crudTableReducer<T>(
-//   draft: Draft<CrudTableState<T>>,
-//   action: CrudTableAction<T>,
-// ): void {
-//   switch (action.type) {
-//     case 'change sort order':
-//       if (action.sort === draft.sort) {
-//         draft.order = draft.order === 'asc' ? 'desc' : 'asc'
-//       } else {
-//         draft.sort = castDraft(action.sort) as any
-//       }
-//       return
-//     case 'select columns':
-//       draft.selectedColumns = castDraft(action.columns)
-//       return
-//     case 'toggle row':
-//       const idx = draft.selectedRows.findIndex(
-//         r => r[action.idKey as any] === action.row[action.idKey],
-//       )
-
-//       if (idx === -1) {
-//         draft.selectedRows.push(castDraft(action.row))
-//       } else {
-//         draft.selectedRows.splice(idx, 1)
-//       }
-//       draft.selectedRows = uniqBy(
-//         draft.selectedRows,
-//         r => r[action.idKey as any],
-//       )
-//       return
-//     case 'change page':
-//       draft.page = action.page
-//       return
-//     // case 'change per page':
-//     //   draft.perPage = action.perPage
-//     //   return
-//   }
-// }
-
-// export function actions<T>(dispatch: React.Dispatch<CrudTableAction<T>>) {
-//   const pageChange = (page: number) => dispatch({ type: 'change page', page })
-
-//   const sortChange = (sort: keyof T) =>
-//     dispatch({ type: 'change sort order', sort })
-
-//   const rowChange = (row: T, idKey: keyof T) =>
-//     dispatch({ type: 'toggle row', row, idKey })
-
-//   return { pageChange, sortChange, rowChange }
-// }
-
-export const dummy = true
+      if (idx === -1) {
+        state.selectedRows.push(castDraft(row))
+      } else {
+        state.selectedRows.splice(idx, 1)
+      }
+      state.selectedRows = uniqBy(state.selectedRows, r => (r as T)[idKey])
+    },
+  })
