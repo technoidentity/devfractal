1fe9fd96cda0c899f9b6bf78d1fa6614d4fb4ba5
sravanidande <sravanidnd89@gmail.com>
2023-02-03 19:34:14 +0530

    ActionsGroup

---
+++ apps/ti-finance/app/components/ActionsGroup.tsx
@@ -0,0 +1,11 @@
+import { Group, Button } from '@mantine/core'
+
+export const AddAction = () => {
+  return (
+    <Group position="center">
+      <Button>Add</Button>
+      {/* <Button variant="outline">2</Button> */}
+      {/* <Button variant="outline">3</Button> */}
+    </Group>
+  )
+}
