e2065a392a94c095b205c8e0e9be1daf182fbfad
sravanidande <sravanidnd89@gmail.com>
2023-02-03 19:22:11 +0530

    e2e for counter

---
+++ e2e/counter.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test'
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3001/')
+  await page.getByRole('button', { name: '+' }).click({
+    clickCount: 4,
+  })
+  await page.getByText('+-').click()
+  await page.getByRole('button', { name: '-' }).click({
+    clickCount: 3,
+  })
+  await page.getByRole('button', { name: '-' }).click()
+  await page.getByRole('button', { name: '-' }).click({
+    clickCount: 4,
+  })
+  await page.getByRole('button', { name: '-' }).click()
+})
