faf948f077f04335dafab3887dd346b3d9bbd968
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-05 23:18:30 +0530

    biased Either

--- libs/core/src/errors/either.ts
+++ libs/core/src/errors/either.ts
@@ -10,10 +10,26 @@ export function left<E, T>(error: E): Either<E, T> {
   return { type: 'left', error }
 }
 
-export function isLeft<E, T>(either: Either<E, T>): boolean {
+export function isLeft<E, T>(either: Either<E, T>): either is Left<E> {
   return either.type === 'left'
 }
 
-export function isRight<E, T>(either: Either<E, T>): boolean {
+export function isRight<E, T>(either: Either<E, T>): either is Right<T> {
   return either.type === 'right'
+}
+
+export function eitherMap<Left, Right, Result>(
+  result: Either<Left, Right>,
+  f: (x: Right) => Result,
+): Either<Left, Result> {
+  return result.type === 'left' ? result : right(f(result.value))
+}
+
+export function eitherFlatMap<Left, Right, Result>(
+  result: Either<Left, Right>,
+  f: (x: Right) => Either<Left, Result>,
+): Either<Left, Result> {
+  const r = eitherMap(result, f)
+
+  return isLeft(r) ? r : r.value
 }
