bdb0cd03e566a88f27529edd624fdb2bc7abde7f
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-07 08:17:27 +0530

    refactor delete user ctc

--- apps/ti-finance/app/components/DeleteUserCtc.tsx
+++ apps/ti-finance/app/components/DeleteUserCtc.tsx
@@ -1,13 +1,12 @@
 import { Button } from '@mantine/core'
 import { Form, useTransition } from '@remix-run/react'
 
-interface DeleteUserCtcProps {
-  id: string
-}
+export type DeleteUserCtcProps = Readonly<{ id: string }>
 
 export const DeleteUserCtc = ({ id }: DeleteUserCtcProps) => {
   const transition = useTransition()
   const deleting = transition.submission
+
   return (
     <Form method="delete">
       <input type="hidden" name="id" value={id} />
