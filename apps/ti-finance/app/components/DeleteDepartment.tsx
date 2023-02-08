496e4eb6f8bf5a0ca8b53551e76a569b7dac4e47
sravanidande <sravanidnd89@gmail.com>
2023-02-08 14:17:11 +0530

    delete department ui

---
+++ apps/ti-finance/app/components/DeleteDepartment.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@mantine/core'
+import { Form, useTransition } from '@remix-run/react'
+
+export type DeleteDepartmentProps = Readonly<{ id: string }>
+
+export const DeleteDepartment = ({ id }: DeleteDepartmentProps) => {
+  const transition = useTransition()
+  const deleting = transition.submission
+
+  return (
+    <Form method="delete">
+      <input type="hidden" name="id" value={id} />
+      <Button type="submit" name="_action" value="delete">
+        {deleting ? 'Deleting' : 'Delete'}
+      </Button>
+    </Form>
+  )
+}
