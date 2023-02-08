77530a5eef72bf12fd33c863afeaffb2ce83021f
sravanidande <sravanidnd89@gmail.com>
2023-02-08 14:13:41 +0530

    seed db with department

--- apps/ti-finance/prisma/seed.ts
+++ apps/ti-finance/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { Billable, PrismaClient } from '@prisma/client'
 import bcrypt from 'bcryptjs'
 
 const prisma = new PrismaClient()
@@ -34,40 +34,40 @@ const tiUsersCtc = [
   },
 ]
 
-// const departments = [
-//   {
-//     id: '1100',
-//     name: 'emma',
-//     department: 'finance',
-//     fromDate: new Date('2017-01-26'),
-//     toDate: new Date('2018-01-26'),
-//     billable: 'nonBillable',
-//   },
-//   {
-//     id: '1101',
-//     name: 'watson',
-//     department: 'finance',
-//     fromDate: new Date('2016-04-14'),
-//     toDate: new Date('2017-04-14'),
-//     billable: 'nonBillable',
-//   },
-//   {
-//     id: '1102',
-//     name: 'reena',
-//     department: 'finance',
-//     fromDate: new Date('2020-06-21'),
-//     toDate: new Date('2021-06-21'),
-//     billable: '',
-//   },
-//   {
-//     id: '1103',
-//     name: 'miland',
-//     department: 'finance',
-//     fromDate: new Date('2021-08-5'),
-//     toDate: new Date('2022-08-5'),
-//     billable: 'nonBillable',
-//   },
-// ]
+const departments = [
+  {
+    id: '1100',
+    name: 'emma',
+    department: 'finance',
+    fromDate: new Date('2017-01-26'),
+    toDate: new Date('2018-01-26'),
+    billable: Billable.billable,
+  },
+  {
+    id: '1101',
+    name: 'watson',
+    department: 'finance',
+    fromDate: new Date('2016-04-14'),
+    toDate: new Date('2017-04-14'),
+    billable: Billable.nonBillable,
+  },
+  {
+    id: '1102',
+    name: 'reena',
+    department: 'finance',
+    fromDate: new Date('2020-06-21'),
+    toDate: new Date('2021-06-21'),
+    billable: Billable.nonBillable,
+  },
+  {
+    id: '1103',
+    name: 'miland',
+    department: 'finance',
+    fromDate: new Date('2021-08-5'),
+    toDate: new Date('2022-08-5'),
+    billable: Billable.billable,
+  },
+]
 
 async function seed() {
   const email = 'rachel@remix.run'
@@ -93,6 +93,12 @@ async function seed() {
   for (let i = 0; i < tiUsersCtc.length; i += 1) {
     await prisma.ctc.create({
       data: tiUsersCtc[i],
+    })
+  }
+
+  for (let i = 0; i < departments.length; i += 1) {
+    await prisma.department.create({
+      data: departments[i],
     })
   }
 
