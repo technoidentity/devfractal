6511325f7d25be7464eb9306dce6fee9dc05afc2
sravanidande <sravanidnd89@gmail.com>
2023-02-08 14:15:51 +0530

    crud db operations for department

---
+++ apps/ti-finance/app/models/department.server.ts
@@ -0,0 +1,56 @@
+import type { Department, PrismaPromise } from '@prisma/client'
+import { Prisma } from '@prisma/client'
+import type { Result } from '@srtp/core'
+import { defaultError, fail, ok } from '@srtp/core'
+import { prisma } from '~/db.server'
+
+export const getDepartments = (): PrismaPromise<Department[]> => {
+  return prisma.department.findMany()
+}
+
+export const createDepartment = async (
+  data: Department,
+): Promise<Result<string, Department>> => {
+  try {
+    const department = await prisma.department.create({ data })
+    return ok(department)
+  } catch (e) {
+    const err =
+      e instanceof Prisma.PrismaClientKnownRequestError && e.code === 'P2002'
+        ? 'user id already exists'
+        : defaultError(e)
+
+    return fail(err)
+  }
+}
+
+export async function deleteDepartment(
+  data: Department,
+): Promise<Result<string, Department>> {
+  try {
+    const dept = await prisma.department.delete({
+      where: {
+        id: data.id,
+      },
+    })
+
+    return ok(dept)
+  } catch (e) {
+    return fail('user not found')
+  }
+}
+
+export async function editDepartment(
+  data: Department,
+): Promise<Result<string, Department>> {
+  try {
+    const result = await prisma.department.update({
+      where: { id: data.id?.toString() },
+      data,
+    })
+
+    return ok(result)
+  } catch (e) {
+    return fail(defaultError(e))
+  }
+}
