13e79c3faece85bb619f0ae4f485d8f2e88f7cac
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-09 20:00:56 +0530

    safeQuery and safeParams remix utils

--- libs/remix-node/src/utils.ts
+++ libs/remix-node/src/utils.ts
@@ -2,7 +2,8 @@ import type { LoaderArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import type { Errors } from '@srtp/remix-core'
 import { formErrors } from '@srtp/remix-core'
-import type { Writeable, z, ZodSchema, ZodType, ZodTypeDef } from 'zod'
+import type { Writeable, ZodSchema, ZodTypeDef } from 'zod'
+import { z, ZodType } from 'zod'
 
 type Request = LoaderArgs['request']
 
@@ -10,11 +11,17 @@ export function badRequest<T extends object>(data: Errors<T>) {
   return json(data, { status: 400 })
 }
 
-export async function formData<Output, Input>(
+export function getSchema<Spec extends z.ZodRawShape | z.ZodTypeAny>(
+  schema: Spec,
+) {
+  return schema instanceof ZodType ? schema : z.object(schema)
+}
+
+export async function safeFormData<Output, Input>(
   spec: ZodType<Output, ZodTypeDef, Input>,
   request: Request,
 ) {
-  const values = Object.fromEntries(await request.formData())
+  const values = Object.fromEntries(await request.clone().formData())
   return spec.safeParse(values)
 }
 
@@ -23,7 +30,7 @@ export async function safeAction<T extends object, R>(
   request: LoaderArgs['request'],
   fn: (values: z.infer<typeof formDataSpec>) => R,
 ) {
-  const result = await formData(formDataSpec, request)
+  const result = await safeFormData(formDataSpec, request)
   if (!result.success) {
     return badRequest({ fieldErrors: formErrors(result.error) })
   }
@@ -39,10 +46,25 @@ export async function safeActions<
     (request: Request) => unknown
   >,
 >(spec: z.ZodEnum<Writeable<T>>, request: Request, actions: Actions) {
-  const rc = request.clone()
-  const actionKey = spec.parse((await rc.formData()).get('_action'))
-  console.log(actionKey, actions)
-  const res = await actions[actionKey](request)
-  console.log('safeActions', res)
-  return res
+  const formData = await request.clone().formData()
+  const actionKey = spec.parse(formData.get('_action'))
+
+  return await actions[actionKey](request)
+}
+
+export function safeParams<Spec extends z.ZodRawShape | z.ZodTypeAny>(
+  schema: Spec,
+  params: LoaderArgs['params'],
+) {
+  return getSchema(schema).parse(params)
+}
+
+// @TODO: use query-string to support arrays etc
+export function safeQuery<Spec extends z.ZodRawShape | z.ZodTypeAny>(
+  schema: Spec,
+  request: Request,
+) {
+  return getSchema(schema).parse(
+    Object.fromEntries(new URL(request.url).searchParams),
+  )
 }
