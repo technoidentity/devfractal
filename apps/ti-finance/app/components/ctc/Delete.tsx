53deb665a072c9ccbd9c16a6bca061feda2d01b9
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-11 18:19:20 +0530

    refactor delete component

--- apps/ti-finance/app/components/ctc/Delete.tsx
+++ apps/ti-finance/app/components/ctc/Delete.tsx
@@ -1,23 +1,14 @@
 import { Button } from '@mantine/core'
-import { Form, useTransition } from '@remix-run/react'
+import { Form } from '@remix-run/react'
+import { Hidden } from '@srtp/remix-react'
 
 export type DeleteUserCtcProps = Readonly<{ id: string }>
 
 export const DeleteCtcForm = ({ id }: DeleteUserCtcProps) => {
-  const transition = useTransition()
-  const deleting = transition.submission
-
   return (
     <Form method="delete">
-      <input type="hidden" name="id" value={id} />
-      <Button
-        type="submit"
-        name="_action"
-        value="delete"
-        style={{ opacity: deleting ? '0.5' : '1' }}
-      >
-        {deleting ? 'Deleting' : 'Delete'}
-      </Button>
+      <Hidden name="id" value={id} />
+      <Button type="submit">Delete</Button>
     </Form>
   )
 }
