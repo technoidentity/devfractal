bf9d67090d8913f9f737db59074248370029f57b
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-11 18:20:57 +0530

    Simple Auth dispatch with SignedIn and SignedOut components

--- apps/ti-finance/app/routes/__index.tsx
+++ apps/ti-finance/app/routes/__index.tsx
@@ -9,6 +9,7 @@ import {
   Title,
 } from '@mantine/core'
 import { Outlet } from '@remix-run/react'
+import React from 'react'
 import { AppHeader } from '~/components/AppHeader'
 import { AppNavbar } from '~/components/AppNavbar'
 import { FooterCentered } from '~/components/Footer'
@@ -25,74 +26,105 @@ const links = [
   },
 ]
 
-export default function Index() {
+const SignedIn = ({ children }: { children: React.ReactNode }) => {
   const user = useOptionalUser()
+
+  if (user) {
+    return <>{children}</>
+  }
+  return null
+}
+
+const SignedOut = ({ children }: { children: React.ReactNode }) => {
+  const user = useOptionalUser()
+
+  if (!user) {
+    return <>{children}</>
+  }
+  return null
+}
+
+const HomePage = () => {
+  const user = useOptionalUser()
+
+  return (
+    <AppShell
+      padding="md"
+      navbar={
+        <Navbar width={{ base: 250 }} p="xs">
+          <AppNavbar />
+        </Navbar>
+      }
+      header={
+        <Header height={60} p="xs">
+          <AppHeader user={user!} />
+        </Header>
+      }
+    >
+      <Outlet />
+    </AppShell>
+  )
+}
+
+const AppIntroPage = () => {
   const { classes, cx } = useStyles()
+
   return (
     <>
-      {user ? (
-        <>
-          <AppShell
-            padding="md"
-            navbar={
-              <Navbar width={{ base: 250 }} p="xs">
-                <AppNavbar />
-              </Navbar>
-            }
-            header={
-              <Header height={60} p="xs">
-                <AppHeader user={user!} />
-              </Header>
-            }
-          >
-            <Outlet />
-          </AppShell>
-        </>
-      ) : (
-        <>
-          <div className={classes.wrapper}>
-            <Overlay color="#000" opacity={0.65} zIndex={1} />
+      <div className={classes.wrapper}>
+        <Overlay color="#000" opacity={0.65} zIndex={1} />
 
-            <div className={classes.inner}>
-              <Title className={classes.title}>
-                Technology, For the people-
-                <Text component="span" inherit className={classes.highlight}>
-                  by the people
-                </Text>
-              </Title>
+        <div className={classes.inner}>
+          <Title className={classes.title}>
+            Technology, For the people-
+            <Text component="span" inherit className={classes.highlight}>
+              by the people
+            </Text>
+          </Title>
 
-              <Container size={640}>
-                <Text size="lg" className={classes.description}>
-                  To advance technology through continuous learning and empathy
-                  so that organizations globally can deliver greater value and
-                  build a better future for humanity.
-                </Text>
-              </Container>
+          <Container size={640}>
+            <Text size="lg" className={classes.description}>
+              To advance technology through continuous learning and empathy so
+              that organizations globally can deliver greater value and build a
+              better future for humanity.
+            </Text>
+          </Container>
 
-              <div className={classes.controls}>
-                <Button
-                  component="a"
-                  href="/join"
-                  className={classes.control}
-                  variant="white"
-                  size="lg"
-                >
-                  Join
-                </Button>
-                <Button
-                  className={cx(classes.control, classes.secondaryControl)}
-                  size="lg"
-                  component="a"
-                  href="/login"
-                >
-                  Sign in
-                </Button>
-              </div>
-            </div>
+          <div className={classes.controls}>
+            <Button
+              component="a"
+              href="/join"
+              className={classes.control}
+              variant="white"
+              size="lg"
+            >
+              Join
+            </Button>
+            <Button
+              className={cx(classes.control, classes.secondaryControl)}
+              size="lg"
+              component="a"
+              href="/login"
+            >
+              Sign in
+            </Button>
           </div>
-          <FooterCentered links={links} />
-        </>
-      )}
+        </div>
+      </div>
+      <FooterCentered links={links} />
+    </>
+  )
+}
+
+export default function Index() {
+  return (
+    <>
+      <SignedIn>
+        <HomePage />
+      </SignedIn>
+      <SignedOut>
+        <AppIntroPage />
+      </SignedOut>
     </>
   )
 }
