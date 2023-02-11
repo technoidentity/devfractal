9d0e75f32e535129f523ea5a7413f4e748bac5ae
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-11 18:19:52 +0530

    extract ctc List component

---
+++ apps/ti-finance/app/components/ctc/List.tsx
@@ -0,0 +1,57 @@
+import { Button, Group } from '@mantine/core'
+import { useActionData } from '@remix-run/react'
+import type { Errors } from '@srtp/remix-core'
+import type { Column, Filters } from '@srtp/table'
+import { ClientTable } from '@srtp/table'
+import type { CtcSchema } from '~/common/validators'
+import { DeleteCtcForm, EditCtcModalForm } from '~/components/ctc'
+
+const columns: Column<CtcSchema>[] = [
+  { accessor: 'id', label: 'TI_ID' },
+  { accessor: 'name', label: 'Username' },
+  { accessor: 'ctc', label: 'CTC(LPA)' },
+  { accessor: 'fromDate', label: 'From Date' },
+  { accessor: 'toDate', label: 'To Date' },
+]
+
+const initialFilters: Filters<CtcSchema> = {
+  id: '',
+  ctc: '',
+  fromDate: '',
+  toDate: '',
+  name: '',
+}
+
+const Actions = ({ row }: { row: CtcSchema }) => {
+  const actionData = useActionData<Errors<CtcSchema> | undefined>()
+
+  return (
+    <Group>
+      <DeleteCtcForm id={row.id} />
+      <EditCtcModalForm ctc={row} errors={actionData} />
+    </Group>
+  )
+}
+
+export type CtcListProps = Readonly<{
+  ctcList: readonly CtcSchema[]
+}>
+
+export const CtcList = ({ ctcList }: CtcListProps) => (
+  <>
+    <Group position="right" m="md">
+      <Button component="a" href="/ctc/new">
+        Add
+      </Button>
+    </Group>
+
+    <ClientTable<CtcSchema>
+      striped
+      Actions={Actions}
+      rows={ctcList}
+      columns={columns}
+      initialFilters={initialFilters}
+      perPage={3}
+    />
+  </>
+)
