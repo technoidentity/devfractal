acb6d76fc95913d68a3aa692eeac645f62dff01b
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-13 00:12:20 +0530

    refactor useTable towards reducer state pattern

--- libs/table/src/useTable.tsx
+++ libs/table/src/useTable.tsx
@@ -1,38 +1,55 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { state } from '@srtp/local-state'
 import { castDraft } from 'immer'
 import { uniqBy } from 'lodash-es'
+import { RowBase } from './types'
 
-export type CrudTableState<T extends object> = Readonly<{
-  sort: keyof T
+export type CrudTableSearchFilters<Row extends RowBase> = Record<
+  keyof Row,
+  string | number | Date | boolean
+>
+
+export type CrudTableState<Row extends RowBase> = Readonly<{
+  sort: keyof Row
   order: 'asc' | 'desc'
-
-  selectedRows: readonly T[]
-  selectedColumns: ReadonlyArray<keyof T>
+  selectedRows: readonly Row[]
+  selectedColumns: ReadonlyArray<keyof Row>
   page: number
-  // search: Record<keyof T, string | number | Date | boolean>
-  // perPage: number
+  search: CrudTableSearchFilters<Row>
+  perPage: number
 }>
 
-export const useCrudTable = <T extends object>(
-  initialState: CrudTableState<T>,
-) =>
-  state(initialState, {
-    changeSortOrder(state, sort: keyof T) {
+export function useTable<Row extends RowBase>(
+  initialState: CrudTableState<Row>,
+) {
+  return state(initialState, {
+    changeSortOrder(state, sort: keyof Row) {
       if (sort === state.sort) {
         state.order = state.order === 'asc' ? 'desc' : 'asc'
       } else {
         state.sort = castDraft(sort)
       }
     },
-    selectColumns(state, columns: ReadonlyArray<keyof T>) {
+
+    selectColumns(state, columns: ReadonlyArray<keyof Row>) {
       state.selectedColumns = castDraft(columns)
     },
+
     changePage(state, page: number) {
       state.page = page
     },
-    toggleRow(state, { row, idKey }: Readonly<{ row: T; idKey: keyof T }>) {
+
+    setPerPage(state, perPage: number) {
+      state.perPage = perPage
+    },
+
+    updateFilters(state, search: Partial<CrudTableSearchFilters<Row>>) {
+      Object.assign(state.search, state.search, search)
+    },
+
+    toggleRow(state, { row, idKey }: Readonly<{ row: Row; idKey: keyof Row }>) {
       const idx = state.selectedRows.findIndex(
-        r => (r as T)[idKey] === row[idKey],
+        r => (r as Row)[idKey] === row[idKey],
       )
 
       if (idx === -1) {
@@ -40,6 +57,8 @@ export const useCrudTable = <T extends object>(
       } else {
         state.selectedRows.splice(idx, 1)
       }
-      state.selectedRows = uniqBy(state.selectedRows, r => (r as T)[idKey])
+
+      state.selectedRows = uniqBy(state.selectedRows, r => (r as Row)[idKey])
     },
   })
+}
