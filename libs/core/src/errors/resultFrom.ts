abe341ae29b49bb9c93307284d6ae41662be737b
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-13 14:11:16 +0530

    generic err call on ResultFrom

--- libs/core/src/errors/resultFrom.ts
+++ libs/core/src/errors/resultFrom.ts
@@ -1,20 +1,20 @@
-import { fail, isFail, Result, resultFlatMap, resultMap } from './result'
+import { fail, isOk, Result, resultFlatMap, resultMap } from './result'
 
 class ResultFrom<E, T> {
   // eslint-disable-next-line no-useless-constructor
   constructor(private readonly res: Result<E, T>) {}
-  map<R>(fn: (x: T) => R) {
-    return new ResultFrom(resultMap(this.res, fn))
+  map<R>(fn: (v: T) => R): ResultFrom<E, R> {
+    return result(resultMap(this.res, fn))
   }
 
-  fmap<R>(fn: (x: T) => Result<E, R>) {
-    return new ResultFrom(resultFlatMap(this.res, fn))
+  fmap<R>(fn: (v: T) => Result<E, R>): ResultFrom<E, R> {
+    return result(resultFlatMap(this.res, fn))
   }
 
-  errMap(fn: (err: E) => E) {
-    return isFail(this.res)
-      ? new ResultFrom(fail<E, T>(fn(this.res.fail)))
-      : this
+  err<E2>(fn: (err: E) => E2): ResultFrom<E2, T> {
+    return isOk(this.res)
+      ? result<E2, T>(this.res)
+      : result(fail<E2, T>(fn(this.res.fail)))
   }
 
   get result() {
@@ -22,6 +22,6 @@ class ResultFrom<E, T> {
   }
 }
 
-export function resultFrom<E, T>(result: Result<E, T>) {
+export function result<E, T>(result: Result<E, T>) {
   return new ResultFrom(result)
 }
