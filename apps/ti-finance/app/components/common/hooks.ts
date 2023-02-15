af669b3b405949e0bb6146c0b8af453f0e074016
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-15 19:42:49 +0530

    useSafeLoader and useFormData for safe access to server data

---
+++ apps/ti-finance/app/components/common/hooks.ts
@@ -0,0 +1,53 @@
+import { useActionData, useLoaderData } from '@remix-run/react'
+import type { Errors } from '@srtp/remix-core'
+import React from 'react'
+import type { z } from 'zod'
+import { createErrorsSpec } from './specs'
+
+export function useLatest<T>(v: T) {
+  const ref = React.useRef(v)
+  React.useEffect(() => {
+    ref.current = v
+  }, [v])
+
+  return ref
+}
+
+export function useSafeLoaderData<Spec extends z.ZodTypeAny>(
+  spec: Spec,
+): z.infer<Spec> {
+  const s = useLatest(spec)
+  const data = useLoaderData()
+  console.log(data)
+
+  return React.useMemo(() => s.current.parse(data), [data, s])
+}
+
+export function useSafeActionData<Spec extends z.ZodTypeAny>(
+  spec: Spec,
+): z.infer<Spec> {
+  const s = useLatest(spec)
+  const data = useActionData()
+
+  return React.useMemo(() => s.current.parse(data), [data, s])
+}
+
+function isEmpty(data: object) {
+  return data === null || data === undefined || Object.keys(data).length === 0
+}
+
+export function useFormData<Spec extends z.AnyZodObject>(
+  spec: Spec,
+): Errors<z.infer<Spec>> {
+  const s = useLatest(createErrorsSpec(spec))
+  const data = useActionData()
+
+  return React.useMemo(() => {
+    //@TODO: currently assuming empty object return from action on success
+    if (isEmpty(data)) {
+      return data
+    }
+
+    return s.current.parse(data)
+  }, [data, s])
+}
