259434c06a319a33af6374171e4dfa7358ca65ac
sravanidande <sravanidnd89@gmail.com>
2023-02-17 00:14:58 +0530

    component for department costing

---
+++ apps/ti-finance/app/components/department/costList.tsx
@@ -0,0 +1,48 @@
+import { Group } from '@mantine/core'
+import { DatePicker } from '@mantine/dates'
+import type { Column, Filters } from '@srtp/table'
+import { Table } from '../common/Table'
+
+type Cost = Readonly<{
+  id: string
+  peopleCost: number
+  otherExpenditures: number
+  totalCost: number
+}>
+
+const columns: Column<Cost>[] = [
+  { accessor: 'id', label: 'Department' },
+  { accessor: 'peopleCost', label: 'People Cost' },
+  { accessor: 'otherExpenditures', label: 'Other Expenditures' },
+  { accessor: 'totalCost', label: 'Total Cost' },
+]
+const initialFilters: Filters<Cost> = {
+  id: '',
+  peopleCost: '',
+  otherExpenditures: '',
+  totalCost: '',
+}
+
+export type DepartmentCostListProps = Readonly<{
+  costList: Cost[]
+}>
+
+export const DepartmentCostList = ({ costList }: DepartmentCostListProps) => {
+  return (
+    <>
+      <Group position="left" m="md">
+        <DatePicker size="xs" label="From Date" />
+        <DatePicker size="xs" label="To Date" />
+        {/* <Select label="Department" data={data} size="xs" /> */}
+      </Group>
+
+      <Table
+        striped
+        rows={costList}
+        columns={columns}
+        initialFilters={initialFilters}
+        perPage={3}
+      />
+    </>
+  )
+}
