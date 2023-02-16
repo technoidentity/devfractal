d17766aed8a7f765cf1575ece42563a98b865cbf
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-16 21:34:29 +0530

    add isDate

--- libs/core/src/types/is.ts
+++ libs/core/src/types/is.ts
@@ -1,4 +1,4 @@
-import { number, string } from 'zod'
+import { date, number, string } from 'zod'
 import { is } from './casts'
 
 // from sindresorhus/is
@@ -33,10 +33,10 @@ export const isStr = isOfType<string>('string')
 export const isFloat = (n: unknown): n is number => isNum(n) && !isInt(n)
 export const isBool = isOfType<boolean>('boolean')
 export const isFunction = isOfType<Function>('function')
-
 export const isEmail = (s: unknown): s is string => is(string().email(), s)
 export const isNum = isOfType<number>('number')
 export const isInt = (s: unknown): s is number => is(number().int(), s)
+export const isDate = (s: unknown): s is Date => is(date(), s)
 
 export const isObject = (value: unknown): value is object =>
   !isNull(value) && (typeof value === 'object' || isFunction(value))
