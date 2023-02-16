f648195397376e341cef120fbdb676a3d5d74445
sravanidande <sravanidnd89@gmail.com>
2023-02-17 00:19:30 +0530

    index page for department

--- apps/ti-finance/app/routes/__index/department/index.tsx
+++ apps/ti-finance/app/routes/__index/department/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Group, Text } from '@mantine/core'
+import { Button, Group, Select, Text } from '@mantine/core'
 import { useLoaderData } from '@remix-run/react'
 import type { ActionArgs, LoaderArgs } from '@remix-run/server-runtime'
 import { json } from '@remix-run/server-runtime'
@@ -7,18 +7,18 @@ import { badRequest, methods, safeAction } from '@srtp/remix-node'
 import { number } from '@srtp/validator'
 import React from 'react'
 import { z } from 'zod'
-// import { capitalizeFirstLetter } from '~/common/stringUtil'
+import { capitalizeFirstLetter } from '~/common/stringUtil'
 
 import { DepartmentMappingSchema } from '~/common/validators'
 import { DepartmentList } from '~/components/department'
 import {
   deleteDepartment,
-  getDepartmentList,
+  getDepartmentMappingsList,
   updateDepartment,
 } from '~/models/department.server'
 
 export async function loader(_: LoaderArgs) {
-  const departments = await getDepartmentList()
+  const departments = await getDepartmentMappingsList()
 
   return json({ departments })
 }
@@ -43,15 +43,17 @@ export const action = (args: ActionArgs) =>
 const DepartmentsPage = () => {
   const { departments } = useLoaderData<typeof loader>()
 
-  // const names = departments.map(d => ({
-  //   label: capitalizeFirstLetter(d.username),
-  //   value: d.username,
-  // }))
+  const names = departments.map(d => ({
+    label: capitalizeFirstLetter(d.username),
+    value: d.username,
+  }))
 
-  // const departmentData = departments.map(d => ({
-  //   label: d.department.toUpperCase(),
-  //   value: d.department,
-  // }))
+  const departmentsData = [...new Set(departments.map(d => d.department))]
+
+  const departmentData = departmentsData.map(d => ({
+    label: d.toUpperCase(),
+    value: d,
+  }))
 
   const departmentList = React.useMemo(
     () => z.array(DepartmentMappingSchema).parse(departments),
@@ -68,8 +70,8 @@ const DepartmentsPage = () => {
           <Text mt="md" fw="bold" size="sm">
             Filter by:{' '}
           </Text>
-          {/* <Select label="Department" data={departmentData} size="xs" /> */}
-          {/* <Select label="Name" data={names} size="xs" /> */}
+          <Select label="Department" data={departmentData} size="xs" />
+          <Select label="Name" data={names} size="xs" />
         </Group>
       </Group>
       <DepartmentList departmentList={departmentList} />
