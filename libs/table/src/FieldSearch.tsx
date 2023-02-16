fa82bfb643e2f9d6b8061d03dabb9dbbe1ccf982
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-16 21:36:12 +0530

    rename Filters to FieldSearch

---
+++ libs/table/src/FieldSearch.tsx
@@ -0,0 +1,30 @@
+import { Input } from '@mantine/core'
+import { RowBase, TableViewProps } from './types'
+
+export type FieldSearchProps<Row extends RowBase> = Required<
+  Pick<TableViewProps<Row>, 'columns' | 'onFieldSearch' | 'fieldSearch'>
+>
+
+export function FieldSearch<Row extends RowBase>({
+  columns,
+  fieldSearch,
+  onFieldSearch,
+}: FieldSearchProps<Row>) {
+  return (
+    <>
+      {columns.map(col => (
+        <th key={col.label}>
+          <Input
+            key={`${col.accessor}-search`}
+            type="search"
+            placeholder={`search ${col.label}`}
+            value={fieldSearch[col.accessor]}
+            onChange={evt =>
+              onFieldSearch(col.accessor, evt.target.value.trim())
+            }
+          />
+        </th>
+      ))}
+    </>
+  )
+}
