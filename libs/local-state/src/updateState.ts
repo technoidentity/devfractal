ee32981db0c9ab48313d0daa40661d4b8c228d03
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-16 21:35:05 +0530

    add updateState to local-state lib

---
+++ libs/local-state/src/updateState.ts
@@ -0,0 +1,14 @@
+import React, { Reducer } from 'react'
+
+export function updateReducer<T>(state: T, update: Partial<T>): T {
+  return { ...state, ...update }
+}
+
+export function useUpdateState<T extends object>(initialState: T) {
+  const [state, update] = React.useReducer(
+    updateReducer as Reducer<T, Partial<T>>,
+    initialState,
+  )
+
+  return [state, update] as const
+}
