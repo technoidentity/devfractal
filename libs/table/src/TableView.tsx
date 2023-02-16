72bc97631027cc9fa857c1474634ced02ad7c10b
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-17 00:27:13 +0530

    sticky header for table-WIP

--- libs/table/src/TableView.tsx
+++ libs/table/src/TableView.tsx
@@ -1,8 +1,36 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { ScrollArea, Table, Text } from '@mantine/core'
+import { createStyles, ScrollArea, Table, Text } from '@mantine/core'
+import React from 'react'
 import { Headers } from './Headers'
 import { Rows } from './Rows'
 import { TableViewProps } from './types'
+
+const useStyles = createStyles(theme => ({
+  header: {
+    position: 'sticky',
+    top: 0,
+    backgroundColor:
+      theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+    transition: 'box-shadow 150ms ease',
+
+    '&::after': {
+      content: '""',
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      bottom: 0,
+      borderBottom: `1px solid ${
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[3]
+          : theme.colors.gray[2]
+      }`,
+    },
+  },
+
+  scrolled: {
+    boxShadow: theme.shadows.sm,
+  },
+}))
 
 export function TableView<T extends { id: number | string } & object>({
   columns,
@@ -14,14 +42,14 @@ export function TableView<T extends { id: number | string } & object>({
   sort,
   ...props
 }: TableViewProps<T>) {
+  const { classes, cx } = useStyles()
+  const [scrolled, setScrolled] = React.useState(false)
+
+  // @TODO: need to provide ScrollArea height for sticky?
   return (
-    <ScrollArea>
-      <Table
-        verticalSpacing="xs"
-        sx={{ tableLayout: 'fixed', minWidth: 700 }}
-        {...props}
-      >
-        <thead>
+    <ScrollArea onScrollPositionChange={({ y }) => setScrolled(y !== 0)}>
+      <Table verticalSpacing="xs" {...props}>
+        <thead className={cx(classes.header, { [classes.scrolled]: scrolled })}>
           <tr>
             <Headers columns={columns} onSort={onSort} sort={sort} />
             {Actions && <th>Actions</th>}
