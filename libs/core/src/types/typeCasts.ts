a3fcad0ffcd4125d8881902273ec3d99b0f715a6
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-18 19:20:27 +0530

    add notNil

--- libs/core/src/types/typeCasts.ts
+++ libs/core/src/types/typeCasts.ts
@@ -1,3 +1,4 @@
+import invariant from 'tiny-invariant'
 import { boolean, number, string } from 'zod'
 import { cast } from './casts'
 
@@ -6,3 +7,8 @@ export const num = (s: unknown): number => cast(number(), s)
 export const int = (s: unknown): number => cast(number().int(), s)
 export const bool = (s: unknown): boolean => cast(boolean(), s)
 export const jstr = (s: unknown): string => JSON.stringify(s, null, 2)
+
+export const notNil = <T>(s: T | null | undefined): T => {
+  invariant(s !== null && s !== undefined, 'value null or undefined')
+  return s
+}
