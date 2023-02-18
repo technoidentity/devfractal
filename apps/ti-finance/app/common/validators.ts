4ae08aeb94d63fdbe3a2e2f90989afe4103da30a
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-19 00:01:42 +0530

    improve ti-finance validators

--- apps/ti-finance/app/common/validators.ts
+++ apps/ti-finance/app/common/validators.ts
@@ -2,51 +2,59 @@ import { Billable } from '@prisma/client'
 import { date, number, positive, schema, string } from '@srtp/validator'
 import { z } from 'zod'
 
+export const IntId = schema({ id: number() })
+type IntId = z.infer<typeof IntId>
+
+export const StrId = schema({ id: number() })
+type StrId = z.infer<typeof StrId>
+
+const BillableSchema = z.nativeEnum(Billable).default('billable')
+
 export const CtcSchema = schema({
-  id: number,
-  tiId: string,
-  ctc: positive,
-  fromDate: date,
-  toDate: date,
+  id: number(),
+  tiId: string(),
+  ctc: positive(),
+  fromDate: date(),
+  toDate: date(),
 })
 
-export const ListCtcSchema = CtcSchema.extend({ name: string })
+export type CtcSchema = z.infer<typeof CtcSchema>
+
+export const ListCtcSchema = CtcSchema.extend({ name: string() })
 
 export type ListCtcSchema = z.infer<typeof ListCtcSchema>
-
-export type CtcSchema = z.infer<typeof CtcSchema>
 
 export const CreateCtcSchema = CtcSchema.omit({ id: true })
 
 export type CreateCtcSchema = z.infer<typeof CreateCtcSchema>
 
-const BillableSchema = z.nativeEnum(Billable)
-
 export const BudgetSchema = schema({
-  id: number,
-  departmentId: number,
+  id: number(),
+  departmentId: number(),
   category: BillableSchema,
-  amount: number.positive(),
-  financialYear: date,
+  amount: positive(),
+  financialYear: date(),
 })
+
+// function specs()
 
 export type BudgetSchema = z.infer<typeof BudgetSchema>
 
 export const ExpenditureSchema = schema({
-  id: number,
-  amount: number.positive(),
-  date: date,
-  category: z.nativeEnum(Billable),
-  remarks: string,
-  departmentId: number,
+  id: number(),
+  amount: positive(),
+  date: date(),
+  category: BillableSchema,
+  remarks: string(),
+  departmentId: number(),
 })
 
 export type ExpenditureSchema = z.infer<typeof ExpenditureSchema>
 
 export const ListExpenditureSchema = ExpenditureSchema.extend({
-  departmentId: number,
-  department: string,
-  id: number,
+  departmentId: number(),
+  department: string(),
+  id: number(),
 })
 
 export type ListExpenditureSchema = z.infer<typeof ListExpenditureSchema>
@@ -58,17 +66,17 @@ export const CreateExpenditureSchema = ExpenditureSchema.omit({
 export type CreateExpenditureSchema = z.infer<typeof CreateExpenditureSchema>
 
 export const DepartmentMappingSchema = schema({
-  id: number,
-  tiId: string,
-  ctc: number.positive(),
-  departmentId: number,
-  fromDate: date,
-  toDate: date,
+  id: number(),
+  tiId: string(),
+  ctc: positive(),
+  departmentId: number(),
+  fromDate: date(),
+  toDate: date(),
   category: BillableSchema,
 })
 
 export const ListDepartmentSchema = DepartmentMappingSchema.extend({
-  username: string,
+  username: string(),
 })
 
 export type ListDepartmentSchema = z.infer<typeof ListDepartmentSchema>
@@ -76,20 +84,20 @@ export type ListDepartmentSchema = z.infer<typeof ListDepartmentSchema>
 export type DepartmentMappingSchema = z.infer<typeof DepartmentMappingSchema>
 
 export const CostSchema = schema({
-  id: string,
-  department: string,
-  peopleCost: number,
-  otherExpenditures: number,
-  totalCost: number,
+  id: string(),
+  department: string(),
+  peopleCost: number(),
+  otherExpenditures: number(),
+  totalCost: number(),
 })
 
 export type CostSchema = z.infer<typeof CostSchema>
 
-export const PeopleSpendSchema = schema({
-  tiId: string,
-  username: string,
-  cost: number.positive(),
-  department: string,
+export const PeopleSpendSchema = z.object({
+  tiId: z.string(),
+  username: z.string(),
+  cost: z.number().int().positive(),
+  department: z.string(),
 })
 
 export type PeopleSpendSchema = z.infer<typeof PeopleSpendSchema>
