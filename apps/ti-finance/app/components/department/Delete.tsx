186ea0c04ac7923af98109ebca206002832a6777
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-19 20:42:14 +0530

    use generic DeleteForm in department

--- apps/ti-finance/app/components/department/Delete.tsx
+++ apps/ti-finance/app/components/department/Delete.tsx
@@ -1,18 +1,3 @@
-import { Button } from '@mantine/core'
-import { Form, useTransition } from '@remix-run/react'
+import { DeleteForm } from '../common'
 
-export type DeleteDepartmentProps = Readonly<{ id: number }>
-
-export const DeleteDepartment = ({ id }: DeleteDepartmentProps) => {
-  const transition = useTransition()
-  const deleting = transition.submission
-
-  return (
-    <Form method="delete">
-      <input type="hidden" name="id" value={id} />
-      <Button type="submit" name="_action" value="delete">
-        {deleting ? 'Deleting' : 'Delete'}
-      </Button>
-    </Form>
-  )
-}
+export const DeleteDepartment = DeleteForm<number>
