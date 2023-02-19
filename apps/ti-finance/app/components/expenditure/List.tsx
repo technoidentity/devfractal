b6f6a07d21fcdf2437891d64ad1186e3722bc347
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-19 22:46:06 +0530

    extract List for expenditure

---
+++ apps/ti-finance/app/components/expenditure/List.tsx
@@ -0,0 +1,55 @@
+import { Button, Group } from '@mantine/core'
+import type { Column } from '@srtp/table'
+import React from 'react'
+import { z } from 'zod'
+import { ExpenditureSchema } from '~/common'
+import { CrudTable, TotalSpendCard } from '~/components/common'
+import { Filters } from '~/components/expenditure'
+import { FormFields } from './FormFields'
+
+type ExpenditureList = ExpenditureSchema & {
+  departmentId: number
+  department: string
+  id: number
+}
+
+const columns: Column<ExpenditureList>[] = [
+  { accessor: 'department', label: 'Department' },
+  { accessor: 'amount', label: 'Amount' },
+  { accessor: 'category', label: 'Category' },
+  { accessor: 'date', label: 'Date' },
+  { accessor: 'remarks', label: 'Remarks' },
+]
+
+export type ExpenditureListProps = Readonly<{
+  expenditureList: ExpenditureList[]
+}>
+
+export const ExpenditureList = ({ expenditureList }: ExpenditureListProps) => {
+  const totalCost = React.useMemo(
+    () => expenditureList.reduce((acc, curr) => acc + curr.amount, 0),
+    [expenditureList],
+  )
+
+  return (
+    <>
+      <TotalSpendCard cost={totalCost} label="Total Amount" />
+
+      <Group position="left" m="md">
+        <Filters />
+
+        <Button component="a" href="/expenditure/new">
+          Add
+        </Button>
+      </Group>
+
+      <CrudTable
+        spec={ExpenditureSchema}
+        editTitle="Update Expenditure"
+        FormFields={FormFields}
+        rows={expenditureList}
+        columns={columns}
+      />
+    </>
+  )
+}
--- apps/ti-finance/app/routes/__index/expenditure/index.tsx
+++ apps/ti-finance/app/routes/__index/expenditure/index.tsx
@@ -1,36 +1,17 @@
-import { Button, Group } from '@mantine/core'
-import { useLoaderData } from '@remix-run/react'
 import type { ActionArgs, LoaderArgs } from '@remix-run/server-runtime'
-import { json } from '@remix-run/server-runtime'
 import { method, methods } from '@srtp/remix-node'
-import type { Column } from '@srtp/table'
-import React from 'react'
-import { z } from 'zod'
-import { ExpenditureSchema, IntId, ListExpenditureSchema } from '~/common'
-import { Table, TotalSpendCard } from '~/components/common'
-import {
-  DeleteExpenditure,
-  EditExpenditureForm,
-  Filters,
-} from '~/components/expenditure'
+import { ExpenditureSchema, IntId, sjson, useGet } from '~/common'
+import { ExpenditureList } from '~/components/expenditure/List'
 import {
   deleteExpenditure,
   getDepartmentExpenditures,
   updateExpenditure,
 } from '~/models/expenditure.server'
 
-const columns: Column<ListExpenditureSchema>[] = [
-  { accessor: 'department', label: 'Department' },
-  { accessor: 'amount', label: 'Amount' },
-  { accessor: 'category', label: 'Category' },
-  { accessor: 'date', label: 'Date' },
-  { accessor: 'remarks', label: 'Remarks' },
-]
-
 export async function loader(_: LoaderArgs) {
   const expenditures = await getDepartmentExpenditures()
 
-  return json({ expenditures })
+  return sjson({ expenditures })
 }
 
 export const action = (args: ActionArgs) => {
@@ -40,49 +21,10 @@ export const action = (args: ActionArgs) => {
   })
 }
 
-const Actions = ({ row }: { row: ListExpenditureSchema }) => {
-  return (
-    <Group>
-      <DeleteExpenditure id={row.id} />
-      <EditExpenditureForm initialValues={row} />
-    </Group>
-  )
-}
-
 const ExpenditurePage = () => {
-  const { expenditures } = useLoaderData<typeof loader>()
+  const { expenditures } = useGet<typeof loader>()
 
-  const expList = React.useMemo(
-    () => z.array(ListExpenditureSchema).parse(expenditures),
-    [expenditures],
-  )
-
-  const totalCost = React.useMemo(
-    () => expList.reduce((acc, curr) => acc + curr.amount, 0),
-    [expList],
-  )
-
-  return (
-    <>
-      <TotalSpendCard cost={totalCost} label="Total Amount" />
-
-      <Group position="left" m="md">
-        <Filters />
-
-        <Button component="a" href="/expenditure/new">
-          Add
-        </Button>
-      </Group>
-
-      <Table
-        striped
-        rows={expList}
-        columns={columns}
-        Actions={Actions}
-        perPage={3}
-      />
-    </>
-  )
+  return <ExpenditureList expenditureList={expenditures} />
 }
 
 export default ExpenditurePage
