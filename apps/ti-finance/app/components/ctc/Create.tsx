214fea534879dfbeb66b2e133555c36d5dfe1dfa
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-19 20:40:05 +0530

    User generic CreateForm in ctc

--- apps/ti-finance/app/components/ctc/Create.tsx
+++ apps/ti-finance/app/components/ctc/Create.tsx
@@ -1,39 +1,23 @@
-import { Box, Paper } from '@mantine/core'
 import type { Errors } from '@srtp/remix-core'
-import { createForm } from '@srtp/remix-react'
 import { CreateCtcSchema } from '~/common/validators'
-import { FormErrors, FormTitle, SubmitButton } from '../common'
+import { CreateForm } from '../common'
 import { FormFields } from './FormFields'
 
-const initialValues: CreateCtcSchema = {
+const initialValues = {
   tiId: '',
   ctc: 0,
   fromDate: new Date(),
   toDate: new Date(),
 }
 
-const { Form, Inputs } = createForm(CreateCtcSchema, initialValues)
-
-export type CreateCtcFormProps = Errors<CreateCtcSchema>
-
-export const CreateCtcForm = (serverErrors: CreateCtcFormProps) => {
+export const CreateCtcForm = (serverErrors: Errors<CreateCtcSchema>) => {
   return (
-    <Paper shadow={'lg'} p="lg" sx={{ maxWidth: 450 }} mt="xl" mx="auto">
-      <FormTitle>Add Employee CTC!</FormTitle>
-
-      <Box>
-        <FormErrors error={serverErrors} />
-
-        <Form
-          initialValues={initialValues}
-          method="post"
-          serverErrors={serverErrors}
-        >
-          <FormFields Inputs={Inputs} />
-
-          <SubmitButton />
-        </Form>
-      </Box>
-    </Paper>
+    <CreateForm
+      FormFields={FormFields}
+      serverErrors={serverErrors}
+      title="Add Employee CTC!"
+      initialValues={initialValues}
+      spec={CreateCtcSchema}
+    />
   )
 }
