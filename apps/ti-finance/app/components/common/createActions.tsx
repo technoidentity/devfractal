35bfb133c6d9b35fa45d2eefae403052afcb5f47
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-19 20:36:57 +0530

    createActions to help CrudTable implementation

---
+++ apps/ti-finance/app/components/common/createActions.tsx
@@ -0,0 +1,39 @@
+import { Group } from '@mantine/core'
+import type { FormSchema } from '@srtp/validator'
+import React from 'react'
+import type { z } from 'zod'
+import { DeleteForm } from './DeleteForm'
+import type { EditFormProps } from './EditForm'
+import { EditForm } from './EditForm'
+import { useFormData } from './hooks'
+
+function createActions$<Spec extends FormSchema & z.AnyZodObject>(
+  spec: Spec,
+  FormFields: EditFormProps<Spec>['FormFields'],
+  editTitle: string,
+) {
+  return function Actions({ row }: { row: z.infer<Spec> }) {
+    const serverErrors = useFormData(spec)
+
+    return (
+      <Group>
+        <DeleteForm id={row.id} />
+        <EditForm
+          FormFields={FormFields}
+          title={editTitle}
+          initialValues={row}
+          spec={spec}
+          serverErrors={serverErrors}
+        />
+      </Group>
+    )
+  }
+}
+
+export function useActions<Spec extends FormSchema & z.AnyZodObject>(
+  spec: Spec,
+  FormFields: EditFormProps<Spec>['FormFields'],
+  editTitle: string,
+) {
+  return React.useMemo(() => createActions$(spec, FormFields, editTitle), [])
+}
