7cbb47b92965fd953a73395aa0cda7e4e1e96fc6
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-19 20:43:17 +0530

    User generic CreateForm in expenditure

--- apps/ti-finance/app/components/expenditure/Create.tsx
+++ apps/ti-finance/app/components/expenditure/Create.tsx
@@ -1,11 +1,7 @@
-import { Box, Button, Center, Group, Paper, Title } from '@mantine/core'
-import type { Department } from '@prisma/client'
 import { Billable } from '@prisma/client'
 import type { Errors } from '@srtp/remix-core'
-import { createForm } from '@srtp/remix-react'
-import React from 'react'
 import { CreateExpenditureSchema } from '~/common/validators'
-import { FormErrors } from '../common'
+import { CreateForm } from '../common'
 import { FormFields } from './FormFields'
 
 const initialValues: CreateExpenditureSchema = {
@@ -16,46 +12,18 @@ const initialValues: CreateExpenditureSchema = {
   remarks: '',
 }
 
-const { Form, Inputs } = createForm(CreateExpenditureSchema, initialValues)
+export type CreateExpenditureFormProps = Errors<CreateExpenditureSchema>
 
-export type ExpenditureFormProps = {
-  error?: string
-  errors?: Errors<CreateExpenditureSchema>
-  departments: ReadonlyArray<Pick<Department, 'id' | 'name'>>
-}
-
-const FormTitle = ({ children }: { children: React.ReactNode }) => (
-  <Center>
-    <Title order={3} mt="xl">
-      {children}
-    </Title>
-  </Center>
-)
-
-const SubmitButton = () => (
-  <Group position="right" mt="xl">
-    <Button type="submit">Submit</Button>
-  </Group>
-)
-
-export const ExpenditureForm = (props: ExpenditureFormProps) => {
+export const CreateExpenditureForm = (
+  serverErrors: CreateExpenditureFormProps,
+) => {
   return (
-    <Paper shadow={'lg'} p="lg" sx={{ maxWidth: 450 }} mt="xl" mx="auto">
-      <FormTitle>Add Department Expenditure</FormTitle>
-
-      <Box>
-        <FormErrors error={props.errors} />
-
-        <Form
-          initialValues={initialValues}
-          method="post"
-          serverErrors={props.errors}
-          onSubmit={console.log}
-        >
-          <FormFields Inputs={Inputs} departments={props.departments} />
-          <SubmitButton />
-        </Form>
-      </Box>
-    </Paper>
+    <CreateForm
+      spec={CreateExpenditureSchema}
+      FormFields={FormFields}
+      title="Create Department Expenditure!"
+      initialValues={initialValues}
+      serverErrors={serverErrors}
+    />
   )
 }
