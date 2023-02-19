c1c63f2e9cfe6722f252e79738bcbbe455876fc0
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-19 20:41:01 +0530

    use Generic Create in department

--- apps/ti-finance/app/components/department/Create.tsx
+++ apps/ti-finance/app/components/department/Create.tsx
@@ -1,9 +1,6 @@
-import { Box, Button, Center, Group, Paper, Title } from '@mantine/core'
 import { Billable } from '@prisma/client'
 import type { Errors } from '@srtp/remix-core'
-import { createForm } from '@srtp/remix-react'
-import React from 'react'
-import { FormErrors } from '../common'
+import { CreateForm } from '../common'
 import { FormFields } from './FormFields'
 import { CreateMappingSchema } from './specs'
 
@@ -16,41 +13,16 @@ const initialValues: CreateMappingSchema = {
   toDate: new Date('12-06-22'),
 }
 
-const { Form, Inputs } = createForm(CreateMappingSchema)
-
 export type CreateDepartmentProps = Errors<CreateMappingSchema>
-
-const FormTitle = ({ children }: { children: React.ReactNode }) => (
-  <Center>
-    <Title order={3} mt="xl">
-      {children}
-    </Title>
-  </Center>
-)
-
-const SubmitButton = () => (
-  <Group position="right" mt="xl">
-    <Button type="submit">Submit</Button>
-  </Group>
-)
 
 export const CreateDepartmentForm = (serverErrors: CreateDepartmentProps) => {
   return (
-    <Paper shadow={'lg'} p="lg" sx={{ maxWidth: 450 }} mt="xl" mx="auto">
-      <FormTitle>Add Employee Department!</FormTitle>
-
-      <Box>
-        <FormErrors error={serverErrors} />
-
-        <Form
-          initialValues={initialValues}
-          method="post"
-          serverErrors={serverErrors}
-        >
-          <FormFields Inputs={Inputs} />
-          <SubmitButton />
-        </Form>
-      </Box>
-    </Paper>
+    <CreateForm
+      spec={CreateMappingSchema}
+      serverErrors={serverErrors}
+      title="Create Department Mapping"
+      FormFields={FormFields}
+      initialValues={initialValues}
+    />
   )
 }
