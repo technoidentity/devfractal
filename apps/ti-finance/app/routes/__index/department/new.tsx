3f4f445671e8a17b4632a2cae1a2522760eeddeb
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-19 22:46:32 +0530

    use safeAction for department new

--- apps/ti-finance/app/routes/__index/department/new.tsx
+++ apps/ti-finance/app/routes/__index/department/new.tsx
@@ -1,26 +1,22 @@
 import type { ActionArgs } from '@remix-run/server-runtime'
 import { redirect } from '@remix-run/server-runtime'
-import { isOk } from '@srtp/core'
-import { formErrors, fromFormData } from '@srtp/remix-core'
-import { badRequest } from '@srtp/remix-node'
+import { isFail } from '@srtp/core'
+import { badRequest, safeAction } from '@srtp/remix-node'
 import { useServerErrors } from '~/components/common'
-import { CreateDepartmentForm } from '~/components/department/Create'
-import { CreateMappingSchema } from '~/components/department/specs'
+import {
+  CreateDepartmentForm,
+  CreateMappingSchema,
+} from '~/components/department'
 import { createDepartmentMapping } from '~/models/departmentMapping.server'
 
-export async function action({ request }: ActionArgs) {
-  const result = await fromFormData(CreateMappingSchema, request)
+export const action = (args: ActionArgs) =>
+  safeAction(CreateMappingSchema, args, async values => {
+    const departmentResult = await createDepartmentMapping(values)
 
-  if (!result.success) {
-    return badRequest({ fieldErrors: formErrors(result.error) })
-  }
-
-  const departmentResult = await createDepartmentMapping(result.data)
-
-  return isOk(departmentResult)
-    ? redirect('/department')
-    : badRequest<CreateMappingSchema>({ error: departmentResult.fail })
-}
+    return isFail(departmentResult)
+      ? badRequest<CreateMappingSchema>({ error: departmentResult.fail })
+      : redirect('/department')
+  })
 
 export const DepartmentPage = () => {
   const actionData = useServerErrors(CreateMappingSchema)
