1b7f3b453b6d0dffdc3f457635c6ba0d9b507b8b
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-20 22:39:12 +0530

    implement filters for spend

--- apps/ti-finance/app/components/spend/Filters.tsx
+++ apps/ti-finance/app/components/spend/Filters.tsx
@@ -3,34 +3,35 @@ import { createInputsGroup } from '@srtp/remix-react'
 import { z } from 'zod'
 import { useUsersSelect } from '~/common'
 
-const FiltersSchema = z.object({
-  fromDate: z.date(),
-  toDate: z.date(),
-  name: z.string(),
+export const SpendFiltersSpec = z.object({
+  dateRange: z.array(z.coerce.date()),
+  tiId: z.string(),
 })
 
-const { Inputs, InputsGroup } = createInputsGroup(FiltersSchema)
+export type SpendFiltersSpec = z.infer<typeof SpendFiltersSpec>
+export type FiltersValues = Partial<SpendFiltersSpec>
 
-export const Filters = () => {
+const { Inputs, InputsGroup } = createInputsGroup(SpendFiltersSpec)
+
+export type SpendFiltersProps = Readonly<{
+  onFilterChange: (values: FiltersValues) => void
+}>
+
+export const Filters = ({ onFilterChange }: SpendFiltersProps) => {
   const userData = useUsersSelect()
 
   return (
-    <InputsGroup onChange={console.log}>
+    <InputsGroup onChange={onFilterChange}>
       <Group position="left" m="md">
-        <Inputs.DatePicker
-          name="fromDate"
-          placeholder="Pick date"
-          label="From date"
-          size="xs"
-        />
-        <Inputs.DatePicker
-          name="toDate"
-          placeholder="Pick date"
-          label="To date"
+        <Inputs.DateRangePicker
+          amountOfMonths={2}
+          name="dateRange"
+          placeholder="Pick from and to date"
+          label="Date range"
           size="xs"
         />
         <Inputs.DynamicSelect
-          name="name"
+          name="tiId"
           size="xs"
           label="Person"
           data={userData}
--- apps/ti-finance/app/routes/__index/spend/index.tsx
+++ apps/ti-finance/app/routes/__index/spend/index.tsx
@@ -1,10 +1,19 @@
 import type { LoaderArgs } from '@remix-run/server-runtime'
+import { safeQuery } from '@srtp/remix-node'
 import { sjson, useGet } from '~/common'
-import { PeopleSpendList } from '~/components/spend'
+import { PeopleSpendList, SpendFiltersSpec } from '~/components/spend'
 import { getPeopleSpend } from '~/models'
 
-export async function loader(_: LoaderArgs) {
-  const { personCost } = await getPeopleSpend()
+export async function loader(args: LoaderArgs) {
+  const q = safeQuery(SpendFiltersSpec.partial(), args.request)
+  const fromDate = { gte: q.dateRange?.[0] ?? undefined }
+  const toDate = { lte: q.dateRange?.[1] ?? undefined }
+
+  const { personCost } = await getPeopleSpend({
+    tiId: q.tiId,
+    fromDate,
+    toDate,
+  })
 
   return sjson({ personCost })
 }
