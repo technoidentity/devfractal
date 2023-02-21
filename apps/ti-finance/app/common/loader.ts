96dd26ceb468c75852dbf85fa20a6bd962f5095c
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-21 23:43:55 +0530

    minor changes to loader

--- apps/ti-finance/app/common/loader.ts
+++ apps/ti-finance/app/common/loader.ts
@@ -1,23 +1,18 @@
 import { useLoaderData } from '@remix-run/react'
 import type { TypedResponse } from '@remix-run/server-runtime'
 import { json } from '@remix-run/server-runtime'
-import superjson from 'superjson'
+import { deserialize, serialize } from 'superjson'
 import type { SuperJSONResult } from 'superjson/dist/types'
 
 export function sjson<T>(data: T, init?: Parameters<typeof json>[1]) {
-  const result = json(superjson.serialize(data), init)
+  const result = json(serialize(data), init)
 
   return result as TypedResponse<SuperJSONResult> & { __srtp__type?: T }
 }
 
-type RT<T extends (...args: any) => any> = T extends (...args: any) => infer R
-  ? R
-  : unknown
-
 export function useGet<F extends (...args: any[]) => any>(): Required<
-  Awaited<RT<F>>
+  Awaited<ReturnType<F>>
 >['__srtp__type'] {
   const data = useLoaderData()
-
-  return superjson.deserialize(data)
+  return deserialize(data)
 }
