98e9610d5c735945d9c9657c903bcdcb1bd947a1
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-22 00:44:51 +0530

    extract SearchForm; common for all search forms

---
+++ apps/ti-finance/app/components/common/SearchForm.tsx
@@ -0,0 +1,44 @@
+import { isEmpty } from '@srtp/core'
+import type { InputsType } from '@srtp/remix-react'
+import { createInputsGroup } from '@srtp/remix-react'
+import React from 'react'
+import type { z } from 'zod'
+import { useSearch } from './hooks'
+
+export type SearchValues<Spec extends z.AnyZodObject> = Partial<z.infer<Spec>>
+
+export type SearchInputsProps<Spec extends z.AnyZodObject> = Readonly<{
+  Inputs: InputsType<Spec>
+}>
+
+export type SearchProps<Spec extends z.AnyZodObject> = Readonly<{
+  spec: Spec
+  SearchFields: (args: SearchInputsProps<Spec>) => JSX.Element
+}>
+
+export function SearchForm<Spec extends z.AnyZodObject>({
+  spec,
+  SearchFields,
+}: SearchProps<Spec>) {
+  const { Inputs, InputsGroup } = React.useMemo(
+    () => createInputsGroup(spec),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  )
+  const [, set] = useSearch(spec)
+
+  const handleSearchChange = React.useCallback(
+    (values: SearchValues<Spec>) => {
+      if (!isEmpty(values)) {
+        set(values)
+      }
+    },
+    [set],
+  )
+
+  return (
+    <InputsGroup onChange={handleSearchChange}>
+      <SearchFields Inputs={Inputs} />
+    </InputsGroup>
+  )
+}
