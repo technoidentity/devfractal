e39bf4df650fed2af69e582f19f5e8392d810df5
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-22 00:43:57 +0530

    CreateForm in terms of CrudForm

--- apps/ti-finance/app/components/common/CreateForm.tsx
+++ apps/ti-finance/app/components/common/CreateForm.tsx
@@ -1,25 +1,17 @@
-import { Box, Paper } from '@mantine/core'
-import type { Errors } from '@srtp/remix-core'
+import { Paper } from '@mantine/core'
 import type { InputsType } from '@srtp/remix-react'
-import { createForm } from '@srtp/remix-react'
 import type { FormSpec } from '@srtp/validator'
-import React from 'react'
-import type { z } from 'zod'
-import { FormErrors, FormTitle, SubmitButton } from '../common'
+import type { CrudFormProps } from './CrudForm'
+import { CrudForm } from './CrudForm'
 
 export type FormFieldsProps<T extends FormSpec> = Readonly<{
   Inputs: InputsType<T>
 }>
 
-export type CreateFormProps<Spec extends FormSpec> = {
-  spec: Spec
-  serverErrors?: Errors<z.infer<Spec>>
-  initialValues: z.infer<Spec>
-  title: string
+export interface CreateFormProps<Spec extends FormSpec>
+  extends Omit<CrudFormProps<Spec>, 'children' | 'onSubmit' | 'method'> {
   FormFields: (props: FormFieldsProps<Spec>) => JSX.Element
 }
-
-export type FormProps<Spec extends FormSpec> = Errors<Spec>
 
 export function CreateForm<Spec extends FormSpec>({
   spec,
@@ -28,29 +20,20 @@ export function CreateForm<Spec extends FormSpec>({
   title,
   FormFields,
 }: CreateFormProps<Spec>) {
-  const { Form, Inputs } = React.useMemo(
-    () => createForm(spec, initialValues),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [],
-  )
-
   return (
     <Paper shadow={'lg'} p="lg" sx={{ maxWidth: 450 }} mt="xl" mx="auto">
-      <FormTitle>{title}</FormTitle>
-
-      <Box>
-        <FormErrors error={serverErrors} />
-
-        <Form
-          initialValues={initialValues}
-          method="post"
-          serverErrors={serverErrors}
-        >
-          <FormFields Inputs={Inputs} />
-
-          <SubmitButton />
-        </Form>
-      </Box>
+      <CrudForm
+        method="post"
+        spec={spec}
+        title={title}
+        initialValues={initialValues}
+        serverErrors={serverErrors}
+        onSubmit={() => {
+          console.log('onSubmit')
+        }}
+      >
+        {FormFields}
+      </CrudForm>
     </Paper>
   )
 }
