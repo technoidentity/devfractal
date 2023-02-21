594b5c0f3c17e8b92f5c30aeffd9668ffcd5a9a9
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-22 00:42:42 +0530

    bring safeQuery here from remix-node

---
+++ apps/ti-finance/app/components/common/safeQuery.ts
@@ -0,0 +1,17 @@
+// @TODO: use query-string to support arrays etc
+import qs from 'query-string'
+import type { z } from 'zod'
+
+// Unfortunately query-string is esm only because of which this function
+// can't be moved to @srtp/remix-node
+export function safeQuery<Spec extends z.ZodTypeAny>(
+  spec: Spec,
+  request: Request,
+  options?: qs.ParseOptions,
+): z.infer<Spec> {
+  const obj = qs.parse(new URL(request.url).searchParams.toString(), {
+    arrayFormat: 'index',
+    ...options,
+  })
+  return spec.parse(obj)
+}
