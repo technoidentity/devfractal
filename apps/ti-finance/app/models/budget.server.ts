6ec9ed0df73a68f7a62664bb8a41540d6498d2cb
sravanidande <sravanidnd89@gmail.com>
2023-02-25 14:42:24 +0530

    partial db query for budget allocation

--- apps/ti-finance/app/models/budget.server.ts
+++ apps/ti-finance/app/models/budget.server.ts
@@ -1,4 +1,8 @@
-import type { BudgetSearchSpec, GetPrismaListType } from '~/common'
+import type {
+  BudgetSearchSpec,
+  BudgetUtilizedSearchSpec,
+  GetPrismaListType,
+} from '~/common'
 import { thisYear } from '~/core'
 import { prisma } from '~/db.server'
 
@@ -39,3 +43,26 @@ export async function getBudgetAllocations(q?: BudgetSearchSpec) {
 }
 
 export type BudgetAllocation = GetPrismaListType<typeof getBudgetAllocations>
+
+export async function getBudgetUtilization(q?: BudgetUtilizedSearchSpec) {
+  const where = getWhere(q)
+  return (
+    await prisma.budget.findMany({
+      select: {
+        id: true,
+        category: true,
+        amount: true,
+        financialYear: true,
+        Department: { select: { id: true, name: true } },
+      },
+      where,
+    })
+  ).map(budget => ({
+    id: budget.id,
+    category: budget.category,
+    amount: budget.amount,
+    department: budget.Department.name,
+    departmentId: budget.Department.id,
+    financialYear: budget.financialYear.getFullYear(),
+  }))
+}
