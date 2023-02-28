c05b16e2db2165950feee33b1b33f323824fc674
Pervez Iqbal <pervezfunctor@gmail.com>
2023-02-28 19:05:35 +0530

    disable eslint rules for next-blog

--- apps/next-blog/.eslintrc.js
+++ apps/next-blog/.eslintrc.js
@@ -4,4 +4,10 @@ module.exports = {
     tsconfigRootDir: __dirname,
   },
   extends: ['srtp'],
+  rules: {
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+  },
 }
