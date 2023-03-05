ad1cb8e47b09cab05006a3d3e6a075175c9f1816
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-06 01:09:08 +0530

    add dbTry for simplifying db access methods

---
+++ apps/ti-finance/app/common/dbTry.ts
@@ -0,0 +1,29 @@
+import { fail, ok, Result, resultMapError } from '@srtp/core'
+import { PrismaClientKnownRequestError } from '~/prisma-client/runtime'
+
+// export class CreateError extends PrismaClientKnownRequestError {}
+
+// export class EditError extends PrismaClientKnownRequestError {}
+
+// export class DeleteError extends PrismaClientKnownRequestError {}
+
+// export class ReadError extends PrismaClientKnownRequestError {}
+
+// export class AuthError extends PrismaClientKnownRequestError {}
+
+type PrismaErr = PrismaClientKnownRequestError
+type Return<T> = Promise<Result<string, T>>
+
+type MapErrorArgs = { mapError?: (e: PrismaClientKnownRequestError) => string }
+
+export async function dbTry<T>(
+  fn: () => Promise<T>,
+  options?: MapErrorArgs,
+): Return<T> {
+  const res = await fn()
+    .then(v => ok<PrismaErr, T>(v))
+    .catch(e => fail<PrismaErr, T>(e))
+
+  const mapError = options?.mapError ?? (e => e.message)
+  return resultMapError(res, mapError)
+}
