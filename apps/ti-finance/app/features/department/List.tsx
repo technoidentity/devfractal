678aa9f6d60b6622d296717321543f448b217e31
sravanidande <sravanidnd89@gmail.com>
2023-03-06 13:27:55 +0530

    List for department access

---
+++ apps/ti-finance/app/features/department/List.tsx
@@ -0,0 +1,46 @@
+import type { Column } from '@srtp/table'
+import { CreateAccessSpec, Table } from '~/common'
+import { CrudTable } from '~/core'
+import type { DepartmentAccessList } from '~/models/department.server'
+import { FormFields } from './FormFields'
+
+const columns: Column<DepartmentAccessList>[] = [
+  { accessor: 'id', label: 'ID' },
+  { accessor: 'name', label: 'Department' },
+  { accessor: 'parentCostCenter', label: 'ParentCostCenter' },
+  { accessor: 'users', label: 'Users' },
+]
+
+export type DepartmentAccessProps = Readonly<{
+  departmentAccessList: readonly DepartmentAccessList[]
+}>
+
+// function Actions({ row }: { row: DepartmentAccessList }) {
+//   const serverErrors = useActionData()
+
+//   return (
+//     <Group noWrap>
+//       {/* <DeleteForm id={row.id} /> */}
+//       {/* <EditForm
+//         FormFields={FormFields}
+//         title="Modify Department Access"
+//         initialValues={row}
+//         spec={CreateDepartmentSpec}
+//         serverErrors={serverErrors}
+//       /> */}
+//     </Group>
+//   )
+// }
+export const DepartmentAccess = ({
+  departmentAccessList,
+}: DepartmentAccessProps) => {
+  return (
+    <CrudTable
+      spec={CreateAccessSpec}
+      FormFields={FormFields}
+      editTitle="Update Department Access Users"
+      rows={departmentAccessList}
+      columns={columns}
+    />
+  )
+}
