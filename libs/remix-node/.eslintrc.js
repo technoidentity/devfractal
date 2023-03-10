daab90f7a67b9d4cc1faf6d3c690eac641a93cea
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-10 23:10:52 +0530

    disable rule no-useless-constructor

--- libs/remix-node/.eslintrc.js
+++ libs/remix-node/.eslintrc.js
@@ -2,6 +2,7 @@
 module.exports = {
   extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node'],
   rules: {
+    'no-useless-constructor': 'off',
     '@typescript-eslint/no-redeclare': 'off',
   },
 }
