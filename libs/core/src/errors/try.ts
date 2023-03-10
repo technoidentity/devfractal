2a1d649d2533d1c79c93bee07e322587f28ed11d
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-10 23:09:52 +0530

    implement try with result

--- libs/core/src/errors/try.ts
+++ libs/core/src/errors/try.ts
@@ -1,40 +1,19 @@
-import type { Either, Left, Right } from './either'
-import { left, right } from './either'
+import type { Result } from './result'
+import { ok, fail } from './result'
 import { toError } from './utils'
 
-export type Try<T> = Either<Error, T>
+export type Try<T> = Result<Error, T>
 
-export function etry<T>(fn: () => T): Try<T> {
+export function rtry<T>(fn: () => T): Try<T> {
   try {
-    return right(fn())
-  } catch (error) {
-    return left(toError(error))
+    return ok(fn())
+  } catch (e) {
+    return fail(toError(e))
   }
 }
 
-export function success<T>(value: T): Try<T> {
-  return { type: 'right', value }
-}
-
-// @TODO: let's take anything and convert to error?
-export function failure<T>(error: unknown): Try<T> {
-  return { type: 'left', error: toError(error) }
-}
-
-export function tryMap<A, R>(result: Try<A>, f: (x: A) => R): Try<R> {
-  return result.type === 'left' ? result : right(f(result.value))
-}
-
-export function tryFlatMap<A, R>(result: Try<A>, f: (x: A) => Try<R>): Try<R> {
-  const r = tryMap(result, f)
-
-  return isFailure(r) ? r : r.value
-}
-
-export function isSuccess<T>(result: Try<T>): result is Right<T> {
-  return result.type === 'right'
-}
-
-export function isFailure<T>(result: Try<T>): result is Left<Error> {
-  return result.type === 'left'
+export function atry<T>(fn: () => Promise<T>): Promise<Try<T>> {
+  return fn()
+    .then(ok)
+    .then(e => fail(toError(e)))
 }
--- libs/core/src/errors/utils.ts
+++ libs/core/src/errors/utils.ts
@@ -3,27 +3,21 @@ import { isObject, isStr, jstr } from '../types'
 import type { Result } from './result'
 import { fail, ok } from './result'
 import type { Try } from './try'
-import { failure as tryFailure, success as trySuccess } from './try'
 
-export const defaultError = (e: unknown) =>
-  e instanceof Error
-    ? e.message
-    : `unexpected prisma error: ${JSON.stringify(e)}`
-
-export function toError(err: unknown): Error {
-  return err instanceof Error
-    ? err
+export const toStringError = (err: unknown): string =>
+  err instanceof Error
+    ? err.message
     : isObject(err) && 'message' in err && isStr(err.message)
-    ? new Error(err.message)
-    : new Error(jstr(err))
-}
+    ? err.message
+    : `unexpected prisma error: ${jstr(err)}`
+
+export const toError = (err: unknown): Error =>
+  err instanceof Error ? err : new Error(toStringError(err))
 
 export function tryFromZod<Output, Input = Output>(
   result: z.SafeParseReturnType<Input, Output>,
 ): Try<Output> {
-  return result.success
-    ? trySuccess(result.data)
-    : tryFailure(toError(result.error))
+  return result.success ? ok(result.data) : fail(result.error)
 }
 
 export function resultFromZod<Output, Input = Output>(
