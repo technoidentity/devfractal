1d6cbfec27ade6e789c1363cb949910ba967079b
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-10 23:09:18 +0530

    simpler function names for result

--- libs/core/src/errors/result.ts
+++ libs/core/src/errors/result.ts
@@ -1,11 +1,10 @@
-import { toError } from './utils'
+import invariant from 'tiny-invariant'
 
 const OK = Symbol('OK')
 const FAIL = Symbol('FAIL')
 
-// @TODO: convert to class?
 export type Ok<T> = Readonly<{ type: typeof OK; value: T }>
-export type Err<E> = Readonly<{ type: typeof FAIL; fail: E }>
+export type Err<E> = Readonly<{ type: typeof FAIL; error: E }>
 export type Result<E, T> = Err<E> | Ok<T>
 
 export function isOk<Error, T>(result: Result<Error, T>): result is Ok<T> {
@@ -22,59 +21,54 @@ export function ok<Error, T>(value: T): Result<Error, T> {
   return { type: OK, value }
 }
 
-export function fail<Error, T>(fail: Error): Result<Error, T> {
-  return { type: FAIL, fail }
+export function fail<Error, T>(error: Error): Result<Error, T> {
+  return { type: FAIL, error }
 }
 
-export function resultMap<E, A, R>(
+export function rmap<E, A, R>(
   result: Result<E, A>,
   f: (x: A) => R,
 ): Result<E, R> {
   return result.type === FAIL ? result : ok(f(result.value))
 }
 
-export function resultFMap<E, A, R>(
+export function rfmap<E, A, R>(
   result: Result<E, A>,
   f: (x: A) => Result<E, R>,
 ): Result<E, R> {
-  const r = resultMap(result, f)
+  const r = rmap(result, f)
 
   return isFail(r) ? r : r.value
 }
 
-export function resultMapError<E, T, E2>(
+export function rmapError<E, T, E2>(
   res: Result<E, T>,
   fn: (err: E) => E2,
 ): Result<E2, T> {
-  return isOk(res) ? res : fail<E2, T>(fn(res.fail))
+  return isOk(res) ? res : fail<E2, T>(fn(res.error))
 }
 
-export function resultExpect<E, T>(res: Result<E, T>, message?: string): T {
-  if (isOk(res)) {
-    return res.value
-  }
-
-  throw new Error(message ?? 'Expected result to be ok')
+export function rvalue$<E, T>(res: Result<E, T>, message?: string): T {
+  invariant(isOk(res), message ?? `Result is not 'ok'`)
+  return res.value
 }
 
-export type ResultMatch<E, T, E2, T2> = {
+export function rerror$<E, T>(res: Result<E, T>, message?: string): E {
+  invariant(isFail(res), message ?? `Result is not 'fail'`)
+
+  return res.error
+}
+
+export type Match<E, T, E2, T2> = {
   ok(x: T): T2
   fail(e: E): E2
 }
 
-export function resultMatch<E, T, E2, T2>(
+export function rmatch<E, T, E2, T2>(
   result: Result<E, T>,
-  match: ResultMatch<E, T, E2, T2>,
+  match: Match<E, T, E2, T2>,
 ): Result<E2, T2> {
   return isOk(result)
     ? ok(match.ok(result.value))
-    : fail(match.fail(result.fail))
-}
-
-export function rtry<T>(fn: () => T): Result<Error, T> {
-  try {
-    return ok(fn())
-  } catch (e) {
-    return fail(toError(e))
-  }
+    : fail(match.fail(result.error))
 }
