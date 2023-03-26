a628f1b4f50ca638f0c4ff3a3db0f129ef6a3432
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-26 19:52:47 +0530

    PrimeGenerator

---
+++ apps/typescript-examples/src/src/examples/PrimeGenerator.ts
@@ -0,0 +1,23 @@
+import invariant from 'tiny-invariant'
+import { isPrime } from './isPrime'
+
+export class PrimeGenerator {
+  private cnt = 0
+  private cur = 2
+  public constructor(private readonly n: number) {}
+
+  hasNext(): boolean {
+    return this.cnt < this.n
+  }
+
+  next(): number | undefined {
+    invariant(this.hasNext())
+
+    while (!isPrime(this.cur)) {
+      this.cur += 1
+    }
+
+    this.cnt += 1
+    return this.cur
+  }
+}
