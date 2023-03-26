60bc041615d2f6b54522aa03a84d3e097fa710e4
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-26 19:53:54 +0530

    LinkedList

---
+++ apps/typescript-examples/src/src/LinkedList.test.ts
@@ -0,0 +1,108 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { expect, test } from 'vitest'
+import { LinkedList } from './LinkedList'
+
+test('push', () => {
+  const list = new LinkedList<number>()
+  list.push(1).push(2).push(3).push(4).push(5)
+
+  expect([...list]).toEqual([1, 2, 3, 4, 5])
+})
+
+test('pop', () => {
+  const list = LinkedList.from([1, 2, 3, 4, 5])
+
+  expect(list.length).toBe(5)
+
+  let i = 0
+  while (list.length > 0) {
+    expect(list.pop()).toBe(5 - i)
+    i += 1
+  }
+})
+
+test('unshift', () => {
+  const list = new LinkedList<number>()
+  list.unshift(1).unshift(2).unshift(3).unshift(4).unshift(5)
+
+  expect([...list]).toEqual([5, 4, 3, 2, 1])
+})
+
+test.only('shift', () => {
+  const list = LinkedList.from([1, 2, 3, 4, 5])
+
+  let i = 0
+  while (list.length > 0) {
+    expect(list.shift()).toBe(i + 1)
+    i += 1
+  }
+
+  expect(list.length).toBe(0)
+})
+
+test('remove, find', () => {
+  const empty = LinkedList.from<number>([])
+  expect(empty.find(1)).toBe(undefined)
+  expect(empty.findBy(v => v === 1)).toBe(undefined)
+
+  const one = LinkedList.from([1])
+  expect(one.first).toBe(1)
+  expect(one.last).toBe(1)
+  expect(one.find(1)).not.toBe(undefined)
+  expect(one.findBy(v => v === 1)).not.toBe(undefined)
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  one.remove(one.find(1)!)
+  expect(one.length).toBe(0)
+  expect(one.first).toBe(undefined)
+  expect(one.last).toBe(undefined)
+
+  const list = LinkedList.from([1, 2, 3, 4, 5])
+
+  let link = list.find(1)
+  link && list.remove(link)
+
+  expect([...list]).toEqual([2, 3, 4, 5])
+
+  link = list.findBy(v => v === 5)
+  link && list.remove(link)
+
+  expect([...list]).toEqual([2, 3, 4])
+
+  link = list.find(3)
+  link && list.remove(link)
+
+  expect([...list]).toEqual([2, 4])
+})
+
+test('insert', () => {
+  const one = LinkedList.from<number>([1])
+  one.insert(one.find(1)!, 2)
+  expect([...one]).toEqual([1, 2])
+
+  const list = LinkedList.from([1, 2, 3, 4, 5])
+
+  let link = list.find(3)
+  expect(link).not.toBe(undefined)
+  if (link) {
+    list.insert(link, 6)
+  }
+
+  expect([...list]).toEqual([1, 2, 6, 3, 4, 5])
+
+  link = list.find(1)
+  expect(link).not.toBe(undefined)
+  if (link) {
+    list.insert(link, 6)
+  }
+
+  expect([...list]).toEqual([6, 1, 2, 6, 3, 4, 5])
+
+  link = list.find(5)
+  expect(link).not.toBe(undefined)
+  if (link) {
+    list.insert(link, 6)
+  }
+
+  expect([...list]).toEqual([6, 1, 2, 6, 3, 4, 5, 6])
+})
---
+++ apps/typescript-examples/src/src/LinkedList.ts
@@ -0,0 +1,150 @@
+type Link<T> = {
+  value: T
+  next: Link<T> | undefined
+  prev: Link<T> | undefined
+}
+
+export class LinkedList<T> {
+  head: Link<T> | undefined
+  tail: Link<T> | undefined
+  len: number
+
+  constructor() {
+    this.head = this.tail = undefined
+    this.len = 0
+  }
+
+  get first(): T | undefined {
+    return this.head?.value
+  }
+
+  get last(): T | undefined {
+    return this.tail?.value
+  }
+
+  push(value: T): LinkedList<T> {
+    const link = { value, next: undefined, prev: this.tail }
+    if (this.tail) {
+      this.tail.next = link
+    } else {
+      this.head = link
+    }
+    this.tail = link
+    this.len += 1
+
+    return this
+  }
+
+  pop(): T | undefined {
+    if (!this.tail) {
+      return undefined
+    }
+
+    const value = this.tail.value
+    this.tail = this.tail.prev
+    if (this.tail) {
+      this.tail.next = undefined
+    } else {
+      this.head = undefined
+    }
+    this.len -= 1
+    return value
+  }
+
+  unshift(value: T): LinkedList<T> {
+    const link = { value, next: this.head, prev: undefined }
+    if (this.head) {
+      this.head.prev = link
+    } else {
+      this.tail = link
+    }
+    this.head = link
+    this.len += 1
+
+    return this
+  }
+
+  shift(): T | undefined {
+    if (!this.head) {
+      return undefined
+    }
+
+    const value = this.head.value
+    this.head = this.head.next
+    if (this.head) {
+      this.head.prev = undefined
+    } else {
+      this.tail = undefined
+    }
+    this.len -= 1
+    return value
+  }
+
+  insert(before: Link<T>, value: T): void {
+    const link = { value, next: before, prev: before.prev }
+    if (before.prev) {
+      before.prev.next = link
+    } else {
+      this.head = link
+    }
+    before.prev = link
+    this.len += 1
+  }
+
+  find(value: T): Link<T> | undefined {
+    let link = this.head
+    while (link) {
+      if (link.value === value) {
+        return link
+      }
+      link = link.next
+    }
+    return undefined
+  }
+
+  findBy(pred: (value: T) => boolean): Link<T> | undefined {
+    let link = this.head
+    while (link) {
+      if (pred(link.value)) {
+        return link
+      }
+      link = link.next
+    }
+    return undefined
+  }
+
+  remove(at: Link<T>): T | undefined {
+    if (at.prev) {
+      at.prev.next = at.next
+    } else {
+      this.head = at.next
+    }
+    if (at.next) {
+      at.next.prev = at.prev
+    } else {
+      this.tail = at.prev
+    }
+    this.len -= 1
+    return at.value
+  }
+
+  *[Symbol.iterator]() {
+    let link = this.head
+    while (link) {
+      yield link.value
+      link = link.next
+    }
+  }
+
+  get length() {
+    return this.len
+  }
+
+  static from<T>(iterable: Iterable<T>): LinkedList<T> {
+    const list = new LinkedList<T>()
+    for (const value of iterable) {
+      list.push(value)
+    }
+    return list
+  }
+}
