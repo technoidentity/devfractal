fc17d8c95038f55dbeb1cfa77c4457fdb0eb638e
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-29 22:31:07 +0530

    fix groupBy

--- apps/typescript-examples/src/groupBy.ts
+++ apps/typescript-examples/src/groupBy.ts
@@ -7,7 +7,7 @@ export function groupBy<T, K extends string | number>(
   for (const v of arr) {
     const k = f(v)
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    result[k] = result[k] === undefined ? [...result[k], v] : [v]
+    result[k] = result[k] !== undefined ? [...result[k], v] : [v]
   }
 
   return result
