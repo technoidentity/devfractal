d7e475b3e355818a4ad0f82f20460d75f0fb8172
Pervez Iqbal <pervezfunctor@gmail.com>
2023-04-09 02:04:53 +0530

    merge sorted linked lists example

---
+++ apps/typescript-examples/src/mergeSortedLists.test.ts
@@ -0,0 +1,27 @@
+import { expect, test } from 'vitest'
+import { LinkedList } from './LinkedList'
+import { linkToIterable, mergeSortedLists } from './mergeSortedLists'
+
+test('mergeSortedLists', () => {
+  const lst = LinkedList.from([1, 13, 24, 35])
+  const snd = LinkedList.from([1, 2, 7, 10, 13, 14, 15, 23, 27, 30, 35])
+
+  expect([...linkToIterable(mergeSortedLists(lst.head, snd.head))]).toEqual([
+    1, 1, 2, 7, 10, 13, 13, 14, 15, 23, 24, 27, 30, 35, 35,
+  ])
+
+  expect([...linkToIterable(mergeSortedLists(undefined, snd.head))]).toEqual([
+    1, 2, 7, 10, 13, 14, 15, 23, 27, 30, 35,
+  ])
+
+  expect([...linkToIterable(mergeSortedLists(lst.head, undefined))]).toEqual([
+    1, 13, 24, 35,
+  ])
+
+  expect([...linkToIterable(mergeSortedLists(undefined, undefined))]).toEqual(
+    [],
+  )
+
+  expect([...lst]).toEqual([1, 13, 24, 35])
+  expect([...snd]).toEqual([1, 2, 7, 10, 13, 14, 15, 23, 27, 30, 35])
+})
---
+++ apps/typescript-examples/src/mergeSortedLists.ts
@@ -0,0 +1,51 @@
+import type { Link } from './link'
+
+type OptionalLink<T> = Link<T> | undefined
+
+export function mergeSortedLists<T>(
+  fst: OptionalLink<T>,
+  snd: OptionalLink<T>,
+): OptionalLink<T> {
+  let current: OptionalLink<T>
+  let head: OptionalLink<T>
+
+  const add = (t: Link<T>) => {
+    const link = { value: t.value, next: undefined, prev: current }
+
+    if (current) {
+      current = current.next = link
+    } else {
+      head = current = link
+    }
+  }
+
+  let temp: Link<T>
+  while (fst && snd) {
+    if (fst.value <= snd.value) {
+      temp = fst
+      fst = fst.next
+    } else {
+      temp = snd
+      snd = snd.next
+    }
+
+    add(temp)
+  }
+
+  for (; fst; fst = fst.next) {
+    add(fst)
+  }
+
+  for (; snd; snd = snd.next) {
+    add(snd)
+  }
+
+  return head
+}
+
+export function* linkToIterable<T>(head: OptionalLink<T>) {
+  while (head !== undefined) {
+    yield head.value
+    head = head.next
+  }
+}
