d89dc8348d004ae5ed79da5d7e4a21f4358ad40a
Pervez Iqbal <pervezfunctor@gmail.com>
2023-05-22 18:39:48 +0530

    usePrimitive example

---
+++ apps/local-state-examples/components/10.Counter.tsx
@@ -0,0 +1,19 @@
+import { Button, Container, HStack, Text } from '@chakra-ui/react'
+import { useInt } from '@srtp/local-state/src'
+import React from 'react'
+
+export const Counter = () => {
+  const [count, setCount] = useInt()
+
+  console.log('render', count)
+
+  return (
+    <Container>
+      <HStack>
+        <Button onClick={() => setCount(count => count + 1)}>+</Button>
+        <Text>{count}</Text>
+        <Button onClick={() => setCount(count => count - 1)}>-</Button>
+      </HStack>
+    </Container>
+  )
+}
