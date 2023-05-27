c1dcb86861a6575e1e32153b4b7afcc9db41509b
Pervez Iqbal <pervezfunctor@gmail.com>
2023-05-28 01:53:22 +0530

    fix formIntents

--- apps/react-router-examples/src/routes/actionForm.ts
+++ apps/react-router-examples/src/routes/actionForm.ts
@@ -1,4 +1,5 @@
 import { parse } from '@conform-to/zod'
+import { isFunction } from '@srtp/spec'
 import { type ActionFunctionArgs, json } from 'react-router-dom'
 import invariant from 'tiny-invariant'
 import type { z } from 'zod'
@@ -21,19 +22,30 @@ export function formAction<Schema extends z.AnyZodObject>(
   }
 }
 
-// @TODO: export function formActions
-
-export async function formActions<
+export async function formIntents<
+  Schema extends z.AnyZodObject,
   U extends string,
   T extends Readonly<[U, ...U[]]>,
   Actions extends Record<
     z.infer<z.ZodEnum<z.Writeable<T>>>,
-    (args: ActionFunctionArgs) => unknown
+    (values: z.infer<Schema>, args: ActionFunctionArgs) => unknown
   >,
->(spec: z.ZodEnum<z.Writeable<T>>, args: ActionFunctionArgs, actions: Actions) {
+>(
+  spec: z.ZodEnum<z.Writeable<T>>,
+  schema: Schema,
+  args: ActionFunctionArgs,
+  actions: Actions,
+) {
   const formData = await args.request.clone().formData()
-  const actionKey = spec.parse(formData.get('_action'))
+  const submission = parse(formData, { schema })
+
+  if (!submission.value) {
+    // @TODO: || submission.intent !== 'submit'
+    return json(submission)
+  }
+
+  const actionKey = spec.parse(submission.intent)
   invariant(actionKey in actions, `${actionKey} not supported`)
 
-  return actions[actionKey](args)
+  return actions[actionKey](submission.value, args)
 }
