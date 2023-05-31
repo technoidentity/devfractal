2d6261f883507eb034b0452d5d21ed1330bc24e8
Pervez Iqbal <pervezfunctor@gmail.com>
2023-05-31 21:24:39 +0530

    add useInputState

---
+++ libs/local-state/src/useInputState.ts
@@ -0,0 +1,38 @@
+import { isFunction } from '@srtp/spec'
+import React from 'react'
+import warning from 'tiny-warning'
+import { useEvent } from './useEvent'
+
+type OnChangeArgs<T> = null | undefined | T | React.ChangeEvent<any>
+type OnChange<T> = (value: OnChangeArgs<T>) => void
+
+export function useInputState<T>(initialState: T): readonly [T, OnChange<T>] {
+  const [value, setValue] = React.useState(initialState)
+
+  const onChange = useEvent((val: OnChangeArgs<T> | ((current: T) => T)) => {
+    if (!val) {
+      setValue(val as T)
+    } else if (isFunction(val)) {
+      setValue(val)
+    } else if (typeof val === 'object' && 'nativeEvent' in val) {
+      const { currentTarget } = val
+
+      warning(
+        currentTarget instanceof HTMLInputElement ||
+          currentTarget instanceof HTMLTextAreaElement ||
+          currentTarget instanceof HTMLSelectElement,
+        "You mostly can't use useInputState with this element.",
+      )
+
+      if (currentTarget.type === 'checkbox') {
+        setValue(currentTarget.checked)
+      } else {
+        setValue(currentTarget.value)
+      }
+    } else {
+      setValue(val)
+    }
+  })
+
+  return [value, onChange] as const
+}
