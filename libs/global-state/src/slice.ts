c4ddf8dfe41889a9476f7aa905775bd0a9cc7974
Pervez Iqbal <pervezfunctor@gmail.com>
2023-06-03 17:35:04 +0530

    slice now returns hooks for actions and value :-D

--- libs/global-state/src/slice.ts
+++ libs/global-state/src/slice.ts
@@ -1,24 +1,39 @@
-import type { Handlers } from '@srtp/local-state'
-import { getReducer } from '@srtp/local-state'
+import type { Actions, Handlers } from '@srtp/local-state'
+import { getActionCreators, getActions, getReducer } from '@srtp/local-state'
 import type { Draft } from 'immer'
 import { produce } from 'immer'
 import { useSetAtom } from 'jotai'
 import { atomWithReducer } from 'jotai/utils'
+import React from 'react'
+import { useValue } from './hooks'
 
 export function signalWithReducer<State, Action>(
   initial: State,
   reducer: (state: Draft<State>, action: Action) => void,
 ) {
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-  const r = produce(reducer) as (state: State, action: Action) => State
+  const r: (state: State, action: Action) => State = produce(reducer)
   return atomWithReducer(initial, r)
 }
 
 export function slice<State extends object, Hs extends Handlers<State>>(
   initial: State,
-  slices: Hs,
+  handlers: Hs,
 ) {
-  return signalWithReducer(initial, getReducer<State, Hs>(slices))
+  const signal = signalWithReducer(initial, getReducer<State, Hs>(handlers))
+
+  const useSignalActions = () => {
+    const dispatch = useDispatch(signal)
+    const actions: Actions<State, Hs> = React.useMemo(
+      () => getActions(getActionCreators(handlers), dispatch),
+      [dispatch],
+    )
+
+    return actions
+  }
+
+  const useSliceValue = () => useValue(signal)
+
+  return [signal, useSignalActions, useSliceValue] as const
 }
 
 export const useDispatch = useSetAtom
