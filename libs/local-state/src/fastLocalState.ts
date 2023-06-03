94116b1f1a514b705f8ef0ae172083340af8bd66
Pervez Iqbal <pervezfunctor@gmail.com>
2023-06-03 17:34:22 +0530

    fastLocalState can use props too now :-)

--- libs/local-state/src/fastLocalState.ts
+++ libs/local-state/src/fastLocalState.ts
@@ -2,48 +2,48 @@ import type { Draft } from 'immer'
 import React from 'react'
 import { useImmerReducer } from 'use-immer'
 
-type ImmerHandler<State> = (state: Draft<State>) => void
+type PHandler<State, Props> = (state: Draft<State>, props: Props) => void
 
-type Handlers<State> = Record<
+export type PHandlers<State, Props> = Record<
   string,
-  (...payload: any[]) => ImmerHandler<State>
+  (...payload: any[]) => PHandler<State, Props>
 >
 
-type Payload<Hs extends Handlers<any>, A extends keyof Hs> = Parameters<
+type Payload<Hs extends PHandlers<any, any>, A extends keyof Hs> = Parameters<
   Hs[A]
 >[number]
 
-type Action<S extends Handlers<any>, A extends keyof S> = Payload<
+type Action<S extends PHandlers<any, any>, A extends keyof S> = Payload<
   S,
   A
 > extends []
   ? Readonly<{ type: A }>
   : Readonly<{ type: A; payload: Payload<S, A> }>
 
-type ReducerAction<State, Hs extends Handlers<State>> = {
+type ReducerAction<State, Props, Hs extends PHandlers<State, Props>> = {
   [A in keyof Hs]: Action<Hs, A>
 }[keyof Hs]
 
-type ActionCreators<State, Hs extends Handlers<State>> = {
+type ActionCreators<State, Props, Hs extends PHandlers<State, Props>> = {
   [A in keyof Hs]: Payload<Hs, A> extends []
     ? () => Action<Hs, A>
     : (payload: Payload<Hs, A>) => Action<Hs, A>
 }
 
-type Actions<State, Hs extends Handlers<State>> = {
+type Actions<State, Props, Hs extends PHandlers<State, Props>> = {
   [A in keyof Hs]: Payload<Hs, A> extends []
     ? () => void
     : (payload: Payload<Hs, A>) => void
 }
 
-type Reducer<State, Hs extends Handlers<State>> = (
+type Reducer<State, Props, Hs extends PHandlers<State, Props>> = (
   state: Draft<State>,
-  action: ReducerAction<State, Hs>,
+  action: ReducerAction<State, Props, Hs>,
 ) => void
 
-function getActionCreators<State, Hs extends Handlers<State>>(
+function getActionCreators<State, Props, Hs extends PHandlers<State, Props>>(
   handlers: Hs,
-): ActionCreators<State, Hs> {
+): ActionCreators<State, Props, Hs> {
   const actionCreators: any = {}
   for (const type of Object.keys(handlers)) {
     actionCreators[type] = (payload: any) => ({ type, payload })
@@ -52,53 +52,58 @@ function getActionCreators<State, Hs extends Handlers<State>>(
   return actionCreators
 }
 
-function getActions<State, Hs extends Handlers<State>>(
-  actionCreators: ActionCreators<State, Hs>,
-  dispatch: React.Dispatch<ReducerAction<State, Hs>>,
+function getActions<State, Props, Hs extends PHandlers<State, Props>>(
+  actionCreators: ActionCreators<State, Props, Hs>,
+  dispatch: React.Dispatch<ReducerAction<State, Props, Hs>>,
 ) {
   const result = {} as any
   for (const type of Object.keys(actionCreators)) {
     result[type] = (...args: any[]) => {
-      dispatch((actionCreators[type] as any)(...args))
+      dispatch((actionCreators[type] as any)(args))
     }
   }
   return result
 }
 
-function getReducer<State, Hs extends Handlers<State>>(
+function getReducer<State, Props, Hs extends PHandlers<State, Props>>(
   handlers: Hs,
-): Reducer<State, Hs> {
+  props: React.MutableRefObject<Props>,
+): Reducer<State, Props, Hs> {
   return (state, action) => {
-    return handlers[action.type](
-      'payload' in action ? action.payload : undefined,
-    )(state)
+    const payload = 'payload' in action ? action.payload : undefined
+    return handlers[action.type](...(payload as any))(state, props.current)
   }
 }
 
-function state$<State, Hs extends Handlers<State>>(
+function state$<State, Props, Hs extends PHandlers<State, Props>>(
   initialState: State,
   handlers: Hs,
-): readonly [Reducer<State, Hs>, ActionCreators<State, Hs>, State] {
+  propsRef: React.MutableRefObject<Props>,
+): readonly [
+  Reducer<State, Props, Hs>,
+  ActionCreators<State, Props, Hs>,
+  State,
+] {
   return [
-    getReducer<State, Hs>(handlers),
-    getActionCreators<State, Hs>(handlers),
+    getReducer<State, Props, Hs>(handlers, propsRef),
+    getActionCreators<State, Props, Hs>(handlers),
     initialState,
   ] as const
 }
 
-function useState$<State, Hs extends Handlers<State>>(
+function useState$<State, Props, Hs extends PHandlers<State, Props>>(
   args: Readonly<
     [
-      reducer: Reducer<State, Hs>,
-      actionCreators: ActionCreators<State, Hs>,
+      reducer: Reducer<State, Props, Hs>,
+      actionCreators: ActionCreators<State, Props, Hs>,
       initialState: State,
     ]
   >,
-): readonly [State, Actions<State, Hs>] {
+): readonly [State, Actions<State, Props, Hs>] {
   const [reducer, actionCreators, initialState] = args
   const [state, dispatch] = useImmerReducer(reducer, initialState)
 
-  const actions: Actions<State, Hs> = React.useMemo(
+  const actions: Actions<State, Props, Hs> = React.useMemo(
     () => getActions(actionCreators, dispatch),
     [actionCreators, dispatch], // None should change
   )
@@ -107,13 +112,25 @@ function useState$<State, Hs extends Handlers<State>>(
 }
 
 // @TODO: support initial state function
-function useState<State, Hs extends Handlers<State>>(
+export function usePState<
+  State,
+  Hs extends PHandlers<State, Props>,
+  Props = {},
+>(
   initialState: State,
   handlers: Hs,
-): readonly [State, Actions<State, Hs>] {
+  props: Props,
+): readonly [State, Actions<State, Props, Hs>] {
+  const propsRef = React.useRef(props)
+
+  // update props ref
+  React.useEffect(() => {
+    propsRef.current = props
+  }, [props])
+
   // deliberate empty dependency array to avoid re-creating the state$ function
   const stateActions = React.useMemo(
-    () => state$<State, Hs>(initialState, handlers),
+    () => state$<State, Props, Hs>(initialState, handlers, propsRef),
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [],
   )
@@ -121,9 +138,12 @@ function useState<State, Hs extends Handlers<State>>(
   return useState$(stateActions)
 }
 
-export function cstate<State, Hs extends Handlers<State>>(
+export function pstate<State, Props, Hs extends PHandlers<State, any>>(
   initialState: State,
   handlers: Hs,
-): () => readonly [State, Actions<State, Hs>] {
-  return () => useState(initialState, handlers)
+) {
+  return (props?: Props, moreState?: Partial<State>) => {
+    const state = { ...initialState, ...moreState }
+    return usePState(state, handlers, props)
+  }
 }
