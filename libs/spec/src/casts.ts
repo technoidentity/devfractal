67561cd51b2aa89ae338aeee804b581164c475c0
Pervez Iqbal <pervezfunctor@gmail.com>
2023-07-17 14:17:36 +0530

    fix casts

--- libs/spec/src/casts.ts
+++ libs/spec/src/casts.ts
@@ -1,26 +1,25 @@
 import type { z } from 'zod'
 
-// @TODO: T shouldn't be here?
-export function cast<T, Spec extends z.ZodType<T>>(
+export function cast<Spec extends z.ZodTypeAny>(
   spec: Spec,
   v: unknown,
 ): z.infer<Spec> {
   return spec.parse(v)
 }
 
-export function ensure<T, Spec extends z.ZodType<T>>(
+export function ensure<Spec extends z.ZodTypeAny>(
   spec: Spec,
   v: unknown,
 ): asserts v is z.infer<Spec> {
   spec.parse(v)
 }
 
-export function debugCast<T, Spec extends z.ZodType<T>>(
+export function debugCast<Spec extends z.ZodTypeAny>(
   spec: Spec,
   v: unknown,
-): T {
+): z.infer<Spec> {
   if (process.env['NODE_ENV'] === 'development') {
     return spec.parse(v)
   }
-  return v as T
+  return v
 }
