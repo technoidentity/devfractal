fa9a36b5880ad82c8a0e268520cc055fcda93dbe
Pervez Iqbal <pervezfunctor@gmail.com>
2023-07-17 14:21:38 +0530

    todo endpoint example

--- apps/fetch-examples/src/fetch/todoEndpoints.ts
+++ apps/fetch-examples/src/fetch/todoEndpoints.ts
@@ -1,14 +1,20 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import { Todo } from '@srtp/todo'
 import { z } from 'zod'
-import { type Endpoint, type ZodPrimitive } from './endpoint'
+import { type EndpointBase } from '@srtp/fetch'
 
 export const todoEndpoints = {
-  getTodos: { path: ['todos'], method: 'get', response: z.array(Todo) },
+  getTodos: {
+    path: ['todos'],
+    response: z.array(Todo),
+    method: 'get',
+  },
 
-  deleteTodo: { path: ['todos', { id: z.number() }], method: 'delete' },
+  removeTodo: {
+    path: ['todos', { id: z.number() }],
+    method: 'delete',
+  },
 
-  createTodo: {
+  addTodo: {
     path: ['todos'],
     method: 'post',
     request: Todo.omit({ id: true }),
@@ -21,40 +27,4 @@ export const todoEndpoints = {
     request: Todo,
     response: Todo,
   },
-} as const satisfies Record<string, Endpoint>
-
-export function restEndpoints<
-  Spec extends z.ZodRawShape & { id: ZodPrimitive },
-  Query extends z.ZodRawShape | z.AnyZodObject,
->(rawSpec: Spec, options: { name: string }, query: Query) {
-  const spec = z.object(rawSpec)
-  const request = query instanceof z.ZodObject ? query : z.object(query)
-
-  const getAll: Endpoint = {
-    path: [options.name],
-    method: 'get',
-    response: z.array(spec),
-    request,
-  }
-
-  const create: Endpoint = {
-    path: [options.name],
-    method: 'post',
-    request: spec.omit({ id: true }),
-    response: Todo,
-  }
-
-  const update: Endpoint = {
-    path: [options.name, { id: rawSpec['id'] }],
-    method: 'put',
-    request: Todo,
-    response: Todo,
-  }
-
-  const remove: Endpoint = {
-    path: [options.name, { id: z.number() }],
-    method: 'delete',
-  }
-
-  return { getAll, create, update, remove }
-}
+} as const satisfies Record<string, EndpointBase>
