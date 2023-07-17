077d663e77a49eccbbc868205e40751cbb15afdb
Pervez Iqbal <pervezfunctor@gmail.com>
2023-07-17 23:41:23 +0530

    endpoint todo api works!

--- apps/fetch-examples/src/fetch/EndpointTodo.tsx
+++ apps/fetch-examples/src/fetch/EndpointTodo.tsx
@@ -1,13 +1,9 @@
-import { ErrorMessage, Loading } from './common'
-
 import { Box, Button, Checkbox, Heading, Input } from '@chakra-ui/react'
+import { createEpApi } from '@srtp/fetch'
 import { useInputState } from '@srtp/local-state'
-import { useMutation } from '@tanstack/react-query'
 import type { KeyboardEvent } from 'react'
-import { createEndpointApi } from './endpoint'
 import { todoEndpoints } from './todoEndpoints'
 import { TodoList, type Todo } from './types'
-import { patch, post } from './utils'
 
 type TodoItemProps = Readonly<{
   todo: Todo
@@ -64,58 +60,51 @@ const AddTodo = ({ onAdd }: AddTodoProps) => {
   )
 }
 
-const todoUrl = '/api/todos'
-const todoIdUrl = (id: number) => `${todoUrl}/${id}`
+// const todoUrl = '/api/todos'
+// const todoIdUrl = (id: number) => `${todoUrl}/${id}`
 
-const api = createEndpointApi(todoEndpoints, '/api')
+const api = createEpApi(todoEndpoints, '/api')
 
-// api.queries.useGetTodos({})
-// api.mutations.useCreateTodo({ body: { title: 'hello', completed: false } })
-// api.mutations.useUpdateTodo({
+// const m = api.useAddTodo({ mutation: { title: 'foo', completed: false } })
+// api.useUpdateTodo({
 //   path: { id: 1 },
 //   body: { id: 1, title: 'hello', completed: false },
 // })
-// api.mutations.useDeleteTodo({ path: { id: 1 } })
+
+// api.useDeleteTodo({ path: { id: 1 } })
 
 export const EndpointTodoApp = () => {
-  const { data, error, refetch } = api.useGetTodos({})
+  const { data, invalidateKey } = api.useGetTodos({})
 
-  const toggleTodo = useMutation({
-    mutationFn: (todo: Todo) =>
-      patch(todoIdUrl(todo.id), { id: todo.id, completed: !todo.completed }),
-    onSettled: () => refetch(),
+  const toggleTodo = api.useUpdateTodo({
+    action: (todo: Todo) => ({
+      path: { id: todo.id },
+      request: { ...todo, completed: !todo.completed },
+      invalidateKey,
+    }),
   })
 
-  const addTodo = useMutation({
-    mutationFn: (title: string) => post(todoUrl, { title, completed: false }),
-    onSettled: () => refetch(),
+  const addTodo = api.useAddTodo({
+    action: (title: string) => ({ request: { title, completed: false } }),
   })
 
-  if (data) {
-    const todoList = TodoList.parse(data)
+  const todoList = TodoList.parse(data)
 
-    return (
-      <>
-        <Heading as="h2" m="2" fontSize="2xl" fontWeight="semibold">
-          Todos List
-        </Heading>
+  return (
+    <>
+      <Heading as="h2" m="2" fontSize="2xl" fontWeight="semibold">
+        Todos List
+      </Heading>
 
-        <AddTodo onAdd={addTodo.mutate} />
+      <AddTodo onAdd={addTodo.mutate} />
 
-        <div>
-          {todoList.map(todo => (
-            <TodoItem key={todo.id} todo={todo} onToggle={toggleTodo.mutate} />
-          ))}
-        </div>
-      </>
-    )
-  }
-
-  if (error) {
-    return <ErrorMessage error={error as Error} />
-  }
-
-  return <Loading />
+      <div>
+        {todoList.map(todo => (
+          <TodoItem key={todo.id} todo={todo} onToggle={toggleTodo.mutate} />
+        ))}
+      </div>
+    </>
+  )
 }
 
 // const api = restQueries(Todo.shape, { name: 'api/todos', query: z.object({}) })
