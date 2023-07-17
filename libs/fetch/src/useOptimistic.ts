99d8fc46f684c9c53f2b430805b08ff4d7097f00
Pervez Iqbal <pervezfunctor@gmail.com>
2023-07-17 14:14:46 +0530

    minor rename

--- libs/fetch/src/useOptimistic.ts
+++ libs/fetch/src/useOptimistic.ts
@@ -1,37 +1,53 @@
 import type {
   MutationFunction,
+  QueryKey,
   UseMutationOptions,
 } from '@tanstack/react-query'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import {
+  useMutation as useMutationImmer,
+  useQueryClient,
+} from '@tanstack/react-query'
 import { produce, type Draft } from 'immer'
 
 import React from 'react'
 import redaxios from 'redaxios'
 import invariant from 'tiny-invariant'
 import type { MutationDescription } from './mutationApi'
-import { mutationApi } from './mutationApi'
+import { ApiDescriptions } from './mutationApi'
 
-export async function mutation<T>(mut: MutationDescription<T>) {
+async function defaultMutation<T>(mut: MutationDescription<T>) {
   const res = await redaxios[mut.type](mut.path, mut.payload)
   return res.data
 }
 
 export type MutationAction<TData, TVariables, TQueryData> = (
   old: Draft<TQueryData>,
-  api: typeof mutationApi,
+  api: typeof ApiDescriptions,
   arg: TVariables,
 ) => MutationDescription<TData>
 
+export type UseOptimisticMutationOptions<
+  TData,
+  TVariables,
+  TQueryData = TData[],
+> = Omit<
+  UseMutationOptions<TData, Error, TVariables, { previous: TQueryData }>,
+  'mutationFn'
+> & { mutation: (mut: MutationDescription<TData>) => Promise<TData> }
+
 export function useDescribeMutation<TData, TVariables, TQueryData>(
-  invalidateQuery: any[],
+  invalidateQuery: QueryKey,
   fn: MutationAction<TData, TVariables, TQueryData>,
+  options?: UseOptimisticMutationOptions<TData, TVariables, TQueryData>,
 ) {
   const result = React.useRef<MutationDescription<TData>>()
 
   const setData = (old: any, values: any): any =>
     produce(old, (draft: any) => {
-      result.current = fn(draft, mutationApi, values)
+      result.current = fn(draft, ApiDescriptions, values)
     })
+
+  const mutation = options?.mutation ?? defaultMutation
 
   const mutationFn = () => {
     invariant(result.current, 'mutation description is undefined')
@@ -42,19 +58,15 @@ export function useDescribeMutation<TData, TVariables, TQueryData>(
     invalidateQuery,
     mutationFn,
     setData,
+    options,
   )
 }
 
-export type MutationOptions<TData, TVariables, TQueryData = TData[]> = Omit<
-  UseMutationOptions<TData, Error, TVariables, { previous: TQueryData }>,
-  'mutationFn'
->
-
 export function useOptimisticValue<TData, TVariables, TQueryData>(
-  invalidateQuery: any[],
+  invalidateQuery: QueryKey,
   mutationFn: MutationFunction<TData, TVariables>,
   setData: (old: TQueryData, newValue: TVariables) => TQueryData,
-  options?: MutationOptions<TData, TVariables, TQueryData>,
+  options?: UseOptimisticMutationOptions<TData, TVariables, TQueryData>,
 ) {
   const qc = useQueryClient()
 
@@ -81,7 +93,7 @@ export function useOptimisticValue<TData, TVariables, TQueryData>(
     qc.setQueryData(invalidateQuery, context?.previous)
   }
 
-  const mutation = useMutation({
+  const mutation = useMutationImmer({
     mutationFn,
     onMutate,
     onError: (err, _, context) => error(err, context),
@@ -98,10 +110,10 @@ export function useOptimisticValue<TData, TVariables, TQueryData>(
 }
 
 export function useOptimistic<TData, TVariables, TQueryData>(
-  invalidateQuery: any[],
+  invalidateQuery: QueryKey,
   mutationFn: MutationFunction<TData, TVariables>,
   setData: (old: Draft<TQueryData>, newValue: TVariables) => void,
-  options?: MutationOptions<TData, TVariables, TQueryData>,
+  options?: UseOptimisticMutationOptions<TData, TVariables, TQueryData>,
 ) {
   return useOptimisticValue<TData, TVariables, TQueryData>(
     invalidateQuery,
