5984428a8d748f33c0a4ddc65644bb5ed481b3a9
Pervez Iqbal <pervezfunctor@gmail.com>
2023-07-21 00:18:26 +0530

    ep router WIP

---
+++ apps/react-router-examples/src/todoEndpoints.ts
@@ -0,0 +1,44 @@
+import type { EndpointBase } from '@srtp/endpoint'
+import {} from '@srtp/endpoint'
+import { boolean, number, string } from '@srtp/validator'
+import { z } from 'zod'
+
+export const Todo = z.object({
+  id: number(),
+  title: string(),
+  completed: boolean(),
+})
+export type Todo = z.infer<typeof Todo>
+
+export const Filters = z.object({
+  page: number().default(1),
+  limit: number().default(10),
+  search: string().optional(),
+  completed: boolean().optional(),
+})
+export type Filters = z.infer<typeof Filters>
+
+export const todoEndpoints = {
+  getTodos: {
+    path: ['todos'],
+    request: Filters,
+    response: z.array(Todo),
+    method: 'get',
+  },
+
+  removeTodo: { path: ['todos', { id: z.coerce.number() }], method: 'delete' },
+
+  addTodo: {
+    path: ['todos'],
+    method: 'post',
+    request: Todo.omit({ id: true }),
+    response: Todo,
+  },
+
+  updateTodo: {
+    path: ['todos', { id: z.coerce.number() }],
+    method: 'patch',
+    request: Todo,
+    response: Todo,
+  },
+} as const satisfies Record<string, EndpointBase>
