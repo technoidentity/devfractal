df9ce087af4027df8d89276c0fcc93dc19e997f3
Pervez Iqbal <pervezfunctor@gmail.com>
2023-07-21 23:09:45 +0530

    prettier

--- apps/conform-examples/vite.config.ts
+++ apps/conform-examples/vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import rollupReplace from '@rollup/plugin-replace';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import rollupReplace from '@rollup/plugin-replace'
 
 export default defineConfig({
-	plugins: [
-		rollupReplace({
-			preventAssignment: true,
-			values: {
-				__DEV__: JSON.stringify(true),
-				'process.env.NODE_ENV': JSON.stringify('development'),
-			},
-		}),
-		react(),
-	],
-});
+  plugins: [
+    rollupReplace({
+      preventAssignment: true,
+      values: {
+        __DEV__: JSON.stringify(true),
+        'process.env.NODE_ENV': JSON.stringify('development'),
+      },
+    }),
+    react(),
+  ],
+})
--- e2e/tests/finance.spec.ts
+++ e2e/tests/finance.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { test } from '@playwright/test'
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:3000/ctc')
