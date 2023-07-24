69d5413de83d8c115bfd7e8f31f96a7241aa3340
Pervez Iqbal <pervezfunctor@gmail.com>
2023-07-24 17:53:52 +0530

    one local storage for the whole project

---
+++ libs/react/src/localStorage.ts
@@ -0,0 +1,23 @@
+import type { z } from 'zod'
+import { useLocalStorage, type LocalStorageResult } from './useLocalStorage'
+
+type Base = Record<string, z.ZodTypeAny>
+
+export type LocalStorageHooks<Obj extends Base> = {
+  [K in keyof Obj as `use${Capitalize<K & string>}`]: (
+    initialValue: z.infer<Obj[K]>,
+  ) => LocalStorageResult<Obj[K]>
+}
+
+export function localStorage<Obj extends Base>(
+  obj: Obj,
+): LocalStorageHooks<Obj> {
+  const result = {} as any
+  for (const key of Object.keys(obj)) {
+    result[key] = function useStorage(initialValue: any) {
+      return useLocalStorage(obj[key], key, initialValue)
+    }
+  }
+
+  return result
+}
