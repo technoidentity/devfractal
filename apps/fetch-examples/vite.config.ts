f06cb8b4b5f6250aec6419d6465e2dac6a581a3d
Pervez Iqbal <pervezfunctor@gmail.com>
2023-07-25 00:07:20 +0530

    make vite process.env aware in fetch examples

--- apps/fetch-examples/vite.config.ts
+++ apps/fetch-examples/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': { NODE_ENV: process.env.NODE_ENV },
+  },
+
   plugins: [react()],
   server: {
     proxy: {
