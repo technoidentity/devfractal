d701e53c62991dff86ac5973cfa772333fd3e8b6
Pervez Iqbal <pervezfunctor@gmail.com>
2023-07-24 17:53:29 +0530

    useSafeState for lower level code

---
+++ libs/react/src/useSafeState.ts
@@ -0,0 +1,28 @@
+import type { z } from 'zod'
+
+import { cast, isFunction } from '@srtp/spec'
+import { useEvent } from './useEvent'
+
+import React from 'react'
+
+// Use this function only to interface with lower level APIs and state is simple.
+export function useSafeState<S extends z.ZodTypeAny>(
+  spec: S,
+  initialValue: z.infer<S> | (() => z.infer<S>),
+) {
+  const init: z.infer<S> = React.useMemo(
+    () => cast(spec, isFunction(initialValue) ? initialValue() : initialValue),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  )
+
+  const [value, set] = React.useState(init)
+
+  const setValue = useEvent(
+    (value: z.infer<S> | ((prev: z.infer<S>) => z.infer<S>)) => {
+      set(prev => cast(spec, isFunction(value) ? value(prev) : value))
+    },
+  )
+
+  return [value, setValue] as const
+}
