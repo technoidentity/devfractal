54482f1471fbc9784e23ff9bed9a2dcb88a447c9
Pervez Iqbal <pervezfunctor@gmail.com>
2023-07-28 01:40:54 +0530

    more information on cast error

--- libs/spec/src/casts.ts
+++ libs/spec/src/casts.ts
@@ -1,9 +1,34 @@
 import type { z } from 'zod'
 
+export class CastError extends Error {
+  constructor(
+    readonly spec: z.ZodTypeAny,
+    readonly object: unknown,
+    readonly zodError: z.ZodError,
+  ) {
+    super(
+      `${zodError.message}\n\n value: ${JSON.stringify(
+        object,
+        null,
+        2,
+      )}\n\n spec: ${JSON.stringify(spec, null, 2)}`,
+    )
+
+    this.name = 'CastError'
+  }
+}
 export function cast<Spec extends z.ZodTypeAny>(
   spec: Spec,
   v: unknown,
 ): z.infer<Spec> {
+  if (process.env.NODE_ENV === 'development') {
+    const result = spec.safeParse(v)
+    if (result.success) {
+      return result.data
+    }
+
+    throw new CastError(spec, v, result.error)
+  }
   return spec.parse(v)
 }
 
@@ -18,7 +43,7 @@ export function debugCast<Spec extends z.ZodTypeAny>(
   spec: Spec,
   v: unknown,
 ): z.infer<Spec> {
-  if (process.env['NODE_ENV'] === 'development') {
+  if (process.env.NODE_ENV === 'development') {
     return spec.parse(v)
   }
   return v
