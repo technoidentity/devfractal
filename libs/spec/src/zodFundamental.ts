5591e464b16f7f26ee83083a8b663d7305265cde
Pervez Iqbal <pervezfunctor@gmail.com>
2023-07-30 00:57:19 +0530

    ZodFundamental must support Default too

--- libs/spec/src/zodFundamental.ts
+++ libs/spec/src/zodFundamental.ts
@@ -1,12 +1,20 @@
 import { z } from 'zod'
 
+// @TODO: handle default values too
 export const Fundamental = z.union([
   z.coerce.string(),
   z.coerce.number(),
   z.coerce.boolean(),
   z.coerce.date(),
   z.coerce.bigint(),
+  z.coerce.string().default(''),
+  z.coerce.number().default(0),
+  z.coerce.boolean().default(false),
+  z.coerce.date().default(new Date(0)),
+  z.coerce.bigint().default(BigInt(0)),
 ])
+
+type Fundamental = typeof Fundamental
 
 export type Primitive = z.infer<typeof Fundamental>
 
@@ -16,6 +24,11 @@ export const ZodFundamental = z.union([
   z.instanceof(z.ZodBoolean),
   z.instanceof(z.ZodDate),
   z.instanceof(z.ZodBigInt),
+  z.instanceof(z.ZodDefault<z.ZodString>),
+  z.instanceof(z.ZodDefault<z.ZodNumber>),
+  z.instanceof(z.ZodDefault<z.ZodBoolean>),
+  z.instanceof(z.ZodDefault<z.ZodDate>),
+  z.instanceof(z.ZodDefault<z.ZodBigInt>),
 ])
 
 export type ZodFundamental = z.infer<typeof ZodFundamental>
