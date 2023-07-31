8822d3465b00a0bbd5b1c66b9bdebd77a9988c3a
Pervez Iqbal <pervezfunctor@gmail.com>
2023-07-31 18:48:49 +0530

    comment out server-examples

--- apps/server-examples/src/basicTodo.ts
+++ apps/server-examples/src/basicTodo.ts
@@ -1,69 +1,71 @@
-import {} from '@srtp/endpoint'
+// import {} from '@srtp/endpoint'
 
-import { Hono } from 'hono'
-import { throwBadRequest, throwCast, throwNotFound } from '@srtp/server'
-import {
-  Filters,
-  Todo,
-  createTodo,
-  deleteTodo,
-  initializeFakeTodoTable,
-  replaceTodo,
-  todoList,
-  updateTodo,
-} from './todoDb'
+// import { Hono } from 'hono'
+// import { throwBadRequest, throwCast, throwNotFound } from '@srtp/server'
+// import {
+//   Filters,
+//   Todo,
+//   createTodo,
+//   deleteTodo,
+//   initializeFakeTodoTable,
+//   replaceTodo,
+//   todoList,
+//   updateTodo,
+// } from './todoDb'
 
-initializeFakeTodoTable()
+// initializeFakeTodoTable()
 
-const app = new Hono()
+// const app = new Hono()
 
-app.get('/todos', c => {
-  const filters = throwCast(Filters, c.req.query())
+// app.get('/todos', c => {
+//   const filters = throwCast(Filters, c.req.query())
 
-  return c.json({ data: todoList(filters) })
-})
+//   return c.json({ data: todoList(filters) })
+// })
 
-app.post('/todos', async c => {
-  const body = throwCast(Todo.omit({ id: true }), await c.req.json())
+// app.post('/todos', async c => {
+//   const body = throwCast(Todo.omit({ id: true }), await c.req.json())
 
-  return c.json({ data: createTodo(body) })
-})
+//   return c.json({ data: createTodo(body) })
+// })
 
-app.put('/todos/:id', async c => {
-  const id = throwCast(Todo.shape.id, c.req.param('id'))
-  const todo = throwCast(Todo, await c.req.json())
+// app.put('/todos/:id', async c => {
+//   const id = throwCast(Todo.shape.id, c.req.param('id'))
+//   const todo = throwCast(Todo, await c.req.json())
 
-  if (id !== todo.id) {
-    throwBadRequest(`id in path(${id}) and body(${todo.id}) must match`)
-  }
+//   if (id !== todo.id) {
+//     throwBadRequest(`id in path(${id}) and body(${todo.id}) must match`)
+//   }
 
-  return c.json({ data: replaceTodo(id, todo) })
-})
+//   return c.json({ data: replaceTodo(id, todo) })
+// })
 
-app.delete('/todos/:id', async c => {
-  const id = throwCast(Todo.shape.id, c.req.param('id'))
+// app.delete('/todos/:id', async c => {
+//   const id = throwCast(Todo.shape.id, c.req.param('id'))
 
-  const todo = deleteTodo(id)
+//   const todo = deleteTodo(id)
 
-  if (!todo) {
-    throwNotFound()
-  }
+//   if (!todo) {
+//     throwNotFound()
+//   }
 
-  return c.json({ data: todo })
-})
+//   return c.json({ data: todo })
+// })
 
-app.patch('/todos/:id', async c => {
-  const id = throwCast(Todo.shape.id, c.req.param('id'))
-  const update = throwCast(
-    Todo.omit({ id: true }).partial(),
-    await c.req.json(),
-  )
+// app.patch('/todos/:id', async c => {
+//   const id = throwCast(Todo.shape.id, c.req.param('id'))
+//   const update = throwCast(
+//     Todo.omit({ id: true }).partial(),
+//     await c.req.json(),
+//   )
 
-  const todo = updateTodo(id, update)
+//   const todo = updateTodo(id, update)
 
-  if (!todo) {
-    throw throwNotFound()
-  }
+//   if (!todo) {
+//     throw throwNotFound()
+//   }
 
-  return c.json({ data: todo })
-})
+//   return c.json({ data: todo })
+// })
+
+export const dummy = 1
