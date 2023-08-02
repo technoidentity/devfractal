b871b65ce95d6cd91043f56875cdfa0db41dd433
Pervez Iqbal <pervezfunctor@gmail.com>
2023-08-03 02:57:57 +0530

    endpoint tests

--- libs/endpoint/src/endpoint.test.ts
+++ libs/endpoint/src/endpoint.test.ts
@@ -1,40 +1,160 @@
-import { expect, test } from 'vitest'
-import { z } from 'zod'
-import { linkfn, paramsSpec, route } from './epFn'
-import { path } from './endpoint'
+/* eslint-disable @typescript-eslint/ban-types */
+import { describe, expect, expectTypeOf, test } from 'vitest'
+import { boolean, number, z } from 'zod'
+import type {
+  GetEpRequest,
+  GetEpResponse,
+  GetParamsArg,
+  GetRequestArg,
+  Params,
+} from './endpoint'
+import {
+  endpoint,
+  ep,
+  epDelete,
+  epGet,
+  epPatch,
+  epPost,
+  epPut,
+  eps,
+} from './endpoint'
 
-// @TODO:
-test('endpoint', () => {
-  const fn = linkfn([
-    'users',
-    { id: z.number() },
-    'posts',
-    { postId: z.number() },
-  ])
+describe('endpoint', () => {
+  test('should return a valid endpoint', () => {
+    const path = ['users'] as const
+    const method = 'get'
+    const response = z.object({ id: z.number() })
+    const request = z.undefined()
+    const expected = endpoint({ path, method, response, request })
+    const result = ep(path, method, response, request)
+    expect(result).toEqual(expected)
+  })
 
-  const r = route([
-    'users',
-    { id: z.number() },
-    'posts',
-    { postId: z.number() },
-  ])
+  test('Params', () => {
+    const spath = ['users', 'todos'] as const
+    type T = Params<typeof spath>
+    expectTypeOf<T>().toEqualTypeOf<{}>()
 
-  expect(r).toEqual('/users/:id/posts/:postId')
-  const url = fn({ id: 1, postId: 2 })
-  expect(url).toEqual('/users/1/posts/2')
-})
+    const path = ['users', { id: number() }, 'todos', { x: boolean() }] as const
+    type T2 = Params<typeof path>
+    expectTypeOf<T2>().toEqualTypeOf<Readonly<{ id: number; x: boolean }>>()
+  })
 
-test('params', () => {
-  const p = path([
-    'hello',
-    { id: z.number() },
-    'world',
-    { a: z.string(), b: z.number() },
-    'universe',
-    { c: z.boolean() },
-  ])
+  test('GetEpResponse', () => {
+    const path = ['users', 'todos'] as const
 
-  const schema = paramsSpec(p)
-  const v = schema.parse({ id: 100, a: 'hello', b: 200, c: true })
-  expect(v).toEqual({ id: 100, a: 'hello', b: 200, c: true })
+    const response = z.object({ id: z.number(), x: z.boolean().optional() })
+    const request = z.string()
+    const ep = epGet(path, response, request)
+    type T = GetEpResponse<typeof ep>
+    expectTypeOf<T>().toEqualTypeOf<{ id: number; x?: boolean | undefined }>()
+
+    const ep2 = epDelete(path)
+    type T2 = GetEpResponse<typeof ep2>
+    expectTypeOf<T2>().toEqualTypeOf<undefined>()
+  })
+
+  test('GetEpRequest', () => {
+    const path = ['users', 'todos'] as const
+
+    const request = z.object({ id: z.number(), x: z.boolean().optional() })
+    const response = z.string()
+    const ep = epGet(path, response, request)
+    type T = GetEpRequest<typeof ep>
+    expectTypeOf<T>().toEqualTypeOf<{ id: number; x?: boolean | undefined }>()
+
+    const ep2 = epDelete(path)
+    type T2 = GetEpRequest<typeof ep2>
+    expectTypeOf<T2>().toEqualTypeOf<undefined>()
+  })
+
+  test('GetRequestArg', () => {
+    const path = ['users', 'todos'] as const
+
+    const request = z.object({ id: z.number(), x: z.boolean().optional() })
+    const response = z.string()
+    const ep = epGet(path, response, request)
+    type T = GetRequestArg<typeof ep>
+    expectTypeOf<T>().toEqualTypeOf<{
+      request: { id: number; x?: boolean | undefined }
+    }>()
+
+    const ep2 = epDelete(path)
+    type T2 = GetRequestArg<typeof ep2>
+    expectTypeOf<T2>().toEqualTypeOf<{ request?: undefined }>()
+  })
+
+  test('GetParamsArg', () => {
+    const path = [
+      'users',
+      { id: z.number() },
+      'todos',
+      { todoId: z.number() },
+    ] as const
+
+    const ep = epDelete(path)
+    type T = GetParamsArg<typeof ep>
+    expectTypeOf<T>().toEqualTypeOf<{
+      readonly params: { readonly id: number; readonly todoId: number }
+    }>()
+
+    const ep2 = epDelete(['todos', 'users'] as const)
+    type T2 = GetParamsArg<typeof ep2>
+    expectTypeOf<T2>().toEqualTypeOf<{ readonly params?: undefined }>()
+  })
+
+  test('eps', () => {
+    const epsObj = eps({
+      getTodos: epGet(['users', 'todos'], z.string(), z.boolean()),
+
+      postTodos: epPost(['users', 'todos'], z.string(), z.string()),
+
+      putTodos: epPut(
+        ['users', 'todos', { id: z.number() }],
+        z.string(),
+        z.string(),
+      ),
+      patchTodos: epPatch(
+        ['users', 'todos', { id: z.number() }],
+        z.string(),
+        z.string(),
+      ),
+      deleteTodos: epDelete(['users', 'todos'], z.string()),
+    })
+
+    type T = typeof epsObj
+
+    expectTypeOf<T>().toEqualTypeOf<{
+      readonly getTodos: Readonly<{
+        method: 'get'
+        path: readonly ['users', 'todos']
+        request: z.ZodBoolean
+        response: z.ZodString
+      }>
+      readonly postTodos: Readonly<{
+        method: 'post'
+        path: readonly ['users', 'todos']
+        request: z.ZodString
+        response: z.ZodString
+      }>
+      readonly putTodos: Readonly<{
+        method: 'put'
+        path: readonly ['users', 'todos', { readonly id: z.ZodNumber }]
+        request: z.ZodString
+        response: z.ZodString
+      }>
+      readonly patchTodos: Readonly<{
+        method: 'patch'
+        path: readonly ['users', 'todos', { readonly id: z.ZodNumber }]
+        request: z.ZodString
+        response: z.ZodString
+      }>
+      readonly deleteTodos: Readonly<{
+        method: 'delete'
+        path: readonly ['users', 'todos']
+        request: z.ZodUndefined
+        response: z.ZodString
+      }>
+    }>()
+  })
 })
---
+++ libs/endpoint/src/epFn.test.ts
@@ -0,0 +1,138 @@
+import { describe, expect, test } from 'vitest'
+import { keysfn, route } from './epFn'
+import { boolean, number, string } from 'zod'
+import { paramsSpec } from './epFn'
+import { z } from 'zod'
+import { linkfn } from './epFn'
+
+describe('keysfn', () => {
+  test('should return an array of keys for a given path', () => {
+    const path = [
+      'users',
+      { id: z.string() },
+      'posts',
+      { postId: z.number() },
+    ] as const
+    const params = { id: '123', postId: 456 }
+    const expectedKeys = ['users', '123', 'posts', 456]
+    expect(keysfn(path, params)).toEqual(expectedKeys)
+  })
+
+  test('should return an array of keys for a given path without params', () => {
+    const path = ['users', 'posts'] as const
+    const expectedKeys = ['users', 'posts']
+    expect(keysfn(path)).toEqual(expectedKeys)
+  })
+})
+
+describe('linkfn', () => {
+  test('should return a function that generates a URL with params', () => {
+    const path = [
+      'users',
+      { id: number() },
+      'posts',
+      { postId: number() },
+    ] as const
+
+    const link = linkfn(path)
+    const url = link({ id: 123, postId: 456 })
+    expect(url).toBe('/users/123/posts/456')
+  })
+
+  test('should return a function that generates a URL without params', () => {
+    const path = ['users', 'posts'] as const
+    const link = linkfn(path)
+    const url = link()
+    expect(url).toBe('/users/posts')
+  })
+})
+
+describe('paramsSpec', () => {
+  test('should return a valid schema for a simple path', () => {
+    const path = [
+      'users',
+      { id: z.number() },
+      'posts',
+      { postId: z.number() },
+    ] as const
+    const schema = paramsSpec(path)
+
+    expect(schema.parse({ id: 1, postId: 2, foo: 100 })).toEqual({
+      id: 1,
+      postId: 2,
+    })
+    expect(() => schema.parse({ id: 1 })).toThrow()
+    expect(() => schema.parse({ postId: 2 })).toThrow()
+    expect(() => schema.parse({ id: '1', postId: 2 })).toThrow()
+    expect(() => schema.parse({})).toThrow()
+  })
+
+  test('should return a valid schema for a path with only one parameter', () => {
+    const path = ['users', { id: z.number() }] as const
+    const schema = paramsSpec(path)
+
+    expect(schema.parse({ id: 1, foo: 100 })).toEqual({ id: 1 })
+    expect(() => schema.parse({ id: '1' })).toThrow()
+    expect(() => schema.parse({})).toThrow()
+  })
+
+  test('should return an empty schema for a path with no parameters', () => {
+    const path = ['users', 'posts'] as const
+    const schema = paramsSpec(path)
+
+    expect(schema.parse({ foo: 100 })).toEqual({})
+    expect(schema.parse({})).toEqual({})
+  })
+})
+
+describe('route', () => {
+  test('should return a simple route', () => {
+    const path = ['users'] as const
+    const expected = '/users'
+    const result = route(path)
+    expect(result).toEqual(expected)
+  })
+
+  test('should return a parameterized route with one parameter', () => {
+    const path = ['users', { id: number() }] as const
+    const expected = '/users/:id'
+    const result = route(path)
+    expect(result).toEqual(expected)
+  })
+
+  test('should return a parameterized route with multiple parameters', () => {
+    const path = [
+      'users',
+      { id: number() },
+      'posts',
+      { postId: string() },
+    ] as const
+    const expected = '/users/:id/posts/:postId'
+    const result = route(path)
+    expect(result).toEqual(expected)
+  })
+
+  test('should return a parameterized route with multiple properties in an object', () => {
+    const path = [
+      'users',
+      { id: number() },
+      'address',
+      { city: string(), state: string() },
+    ] as const
+    const expected = '/users/:id/address/:city/:state'
+    const result = route(path)
+    expect(result).toEqual(expected)
+  })
+
+  test('should return a parameterized route with a boolean', () => {
+    const path = [
+      'users',
+      { id: number() },
+      'isAdmin',
+      { isAdmin: boolean() },
+    ] as const
+    const expected = '/users/:id/isAdmin/:isAdmin'
+    const result = route(path)
+    expect(result).toEqual(expected)
+  })
+})
