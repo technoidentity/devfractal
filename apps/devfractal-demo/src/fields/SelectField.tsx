8015b2aa2a46510cd4b0edcd0c1e9cbc4d37caf5
Pervez Iqbal <pervezfunctor@gmail.com>
2023-08-08 02:30:50 +0530

    SelectField compatible with @mantine/form

--- apps/devfractal-demo/src/fields/SelectField.tsx
+++ apps/devfractal-demo/src/fields/SelectField.tsx
@@ -1,9 +1,27 @@
-import { FormField, useControllerProps } from '@/ui/form'
+import {
+  AriaControl,
+  Controller,
+  FormDescription,
+  FormField,
+  FormLabel,
+  FormMessage,
+} from '@/ui/form'
 import { Select, SelectContent, SelectTrigger, SelectValue } from '@/ui/select'
-import { AriaControl, FormDescription, FormLabel, FormMessage } from '@/ui/form'
 import type { BaseFieldProps } from './common'
 
-export type SelectFieldProps = React.ComponentProps<typeof Select> &
+type SelectBaseProps = Omit<
+  React.ComponentProps<typeof Select>,
+  'onValueChange'
+> & { onChange?: (value: string) => void }
+
+const SelectBase = ({ onChange, ...props }: SelectBaseProps) => (
+  <Select {...props} onValueChange={onChange} />
+)
+
+export type SelectFieldProps = Omit<
+  React.ComponentProps<typeof Select>,
+  'value' | 'onValueChange'
+> &
   BaseFieldProps &
   Readonly<{
     className?: string
@@ -23,25 +41,24 @@ export const SelectField = ({
   children,
   ...props
 }: SelectFieldProps) => {
-  const field = useControllerProps()
-
   return (
     <FormField name={name}>
       {label && <FormLabel className={cnLabel}>{label}</FormLabel>}
-      <Select {...props} onValueChange={field.onChange} value={field.value}>
-        <AriaControl className={cnField}>
-          <SelectTrigger>
-            <SelectValue placeholder={placeholder} className={className} />
-          </SelectTrigger>
-        </AriaControl>
-        <SelectContent>{children}</SelectContent>
-      </Select>
+      <Controller>
+        <SelectBase {...props}>
+          <AriaControl className={cnField}>
+            <SelectTrigger>
+              <SelectValue placeholder={placeholder} className={className} />
+            </SelectTrigger>
+          </AriaControl>
+          <SelectContent>{children}</SelectContent>
+        </SelectBase>
+      </Controller>
       {description && (
         <FormDescription className={cnDescription}>
           {description}
         </FormDescription>
       )}
-
       <FormMessage className={cnMessage} />
     </FormField>
   )
