e3fd3a0774451be64ae954a0bac428d0b0f6f6e1
Pervez Iqbal <pervezfunctor@gmail.com>
2023-08-07 13:34:21 +0530

    BaseFieldProps

---
+++ packages/devfractal-demo/src/examples/ui/common.ts
@@ -0,0 +1,9 @@
+export type BaseFieldProps = Readonly<{
+  name: string
+  label?: React.ReactNode
+  description?: React.ReactNode
+  cnField?: string
+  cnLabel?: string
+  cnMessage?: string
+  cnDescription?: string
+}>
