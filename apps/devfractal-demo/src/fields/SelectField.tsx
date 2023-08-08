f52221bfc2d1cec543b417a68c259c4b7362a06b
Pervez Iqbal <pervezfunctor@gmail.com>
2023-08-09 01:54:00 +0530

    extract reusable Base component from SelectField

--- apps/devfractal-demo/src/fields/SelectField.tsx
+++ apps/devfractal-demo/src/fields/SelectField.tsx
@@ -2,64 +2,83 @@ import {
   AriaControl,
   Controller,
   FormDescription,
-  FormField,
+  Field,
   FormLabel,
   FormMessage,
 } from '@/ui/form'
-import { Select, SelectContent, SelectTrigger, SelectValue } from '@/ui/select'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/ui/select'
 import type { BaseFieldProps } from './common'
+import { cn } from '@/core'
 
-type SelectBaseProps = Omit<
+type SelectInternalProps = Omit<
   React.ComponentProps<typeof Select>,
   'onValueChange'
 > & { onChange?: (value: string) => void }
 
-const SelectBase = ({ onChange, ...props }: SelectBaseProps) => (
-  <Select {...props} onValueChange={onChange} />
-)
+const SelectInternal = ({ onChange, value, ...props }: SelectInternalProps) => {
+  return <Select {...props} onValueChange={onChange} />
+}
 
-export type SelectFieldProps = Omit<
+type SelectBaseProps = Omit<
   React.ComponentProps<typeof Select>,
   'value' | 'onValueChange'
 > &
-  BaseFieldProps &
-  Readonly<{
-    className?: string
-    placeholder: string
-  }>
+  Readonly<{ className?: string; placeholder: string; cnTrigger?: string }>
+
+export const SelectBase = ({
+  cnTrigger,
+  placeholder,
+  children,
+  ...props
+}: SelectBaseProps) => {
+  return (
+    <Controller>
+      <SelectInternal {...props}>
+        <AriaControl>
+          <SelectTrigger className={cn('w-60', cnTrigger)}>
+            <SelectValue placeholder={placeholder} />
+          </SelectTrigger>
+        </AriaControl>
+        <SelectContent>{children}</SelectContent>
+      </SelectInternal>
+    </Controller>
+  )
+}
+
+export type SelectFieldProps = SelectBaseProps & BaseFieldProps
 
 export const SelectField = ({
   className,
-  placeholder,
   name,
   label,
   description,
+  cnTrigger,
   cnField,
   cnLabel,
   cnMessage,
   cnDescription,
-  children,
   ...props
 }: SelectFieldProps) => {
   return (
-    <FormField name={name}>
+    <Field name={name} className={className}>
       {label && <FormLabel className={cnLabel}>{label}</FormLabel>}
-      <Controller>
-        <SelectBase {...props}>
-          <AriaControl className={cnField}>
-            <SelectTrigger>
-              <SelectValue placeholder={placeholder} className={className} />
-            </SelectTrigger>
-          </AriaControl>
-          <SelectContent>{children}</SelectContent>
-        </SelectBase>
-      </Controller>
+
+      <SelectBase {...props} cnTrigger={cnTrigger} className={cnField} />
+
       {description && (
         <FormDescription className={cnDescription}>
           {description}
         </FormDescription>
       )}
+
       <FormMessage className={cnMessage} />
-    </FormField>
+    </Field>
   )
 }
+SelectField.Item = SelectItem
