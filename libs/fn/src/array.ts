483b99858b1b11c058ddee0ecdaa900af95f0743
Pervez Iqbal <pervezfunctor@gmail.com>
2023-08-21 00:25:15 +0530

    minor fixes

--- libs/fn/src/array.ts
+++ libs/fn/src/array.ts
@@ -113,7 +113,7 @@ export function arrayEqual<T>(fst: readonly T[]) {
 type DeepFlattenArgs<T> = ReadonlyArray<T | DeepFlattenArgs<T>>
 
 export function deepFlatten<T>(arr: DeepFlattenArgs<T>): T[] {
-  const isArray = (x: unknown): x is DeepFlattenArgs<any> => isArray(x)
+  const isArray = (x: unknown): x is DeepFlattenArgs<any> => Array.isArray(x)
 
   const result: T[] = []
   for (const e of arr) {
