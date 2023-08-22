a4c71ac06d0703f61001cb278e45bb13a0243334
Pervez Iqbal <pervezfunctor@gmail.com>
2023-08-22 13:11:24 +0530

    drop fastify vanilla project

--- starters/fastify-vanilla/src/routes/ping.ts
+++
@@ -1,7 +0,0 @@
-import type { FastifyPluginAsync } from 'fastify'
-
-export const ping: FastifyPluginAsync = async (fastify): Promise<void> => {
-  fastify.get('/ping', async function () {
-    return 'pong\n'
-  })
-}
