d61fea913f00d058fad7d413d224b52e0adc604c
Pervez Iqbal <pervezfunctor@gmail.com>
2023-09-03 20:42:33 +0530

    enable query developer tools in dev mode

--- libs/query/src/core/QueryProvider.tsx
+++ libs/query/src/core/QueryProvider.tsx
@@ -1,3 +1,4 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 
@@ -30,6 +31,11 @@ export const QueryProvider = ({
   )
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      {process.env.NODE_ENV === 'development' && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
+    </QueryClientProvider>
   )
 }
