82e68b41ab8edfbbc46fefaab3cf36864a093f9e
Pervez Iqbal <pervezfunctor@gmail.com>
2023-09-03 20:44:40 +0530

    refactor list query functions

--- libs/query/src/review/index.ts
+++ libs/query/src/review/index.ts
@@ -1,1 +1,2 @@
 export * from './queryState'
+export * from './listQuery'
--- libs/query/src/review/listQuery.ts
+++ libs/query/src/review/listQuery.ts
@@ -1,17 +1,27 @@
-import type { MutationFunction, QueryKey } from '@tanstack/react-query'
+import type {
+  MutationFunction,
+  QueryKey,
+  UseMutationOptions,
+} from '@tanstack/react-query'
 import type { z } from 'zod'
 
 import { useSafeMutation, type UseOptimisticMutationOptions } from '../core'
 
+type IDType = { id: any }
+export type PostMutationValue<T extends IDType> = Omit<T, 'id'>
+export type PatchMutationValue<T extends IDType> = Pick<T, 'id'> &
+  Partial<Omit<T, 'id'>>
+
 type Options<TData, TVariables> = Omit<
-  UseOptimisticMutationOptions<TVariables, TVariables, TData[]>,
-  'setData'
+  UseMutationOptions<TData, Error, TVariables, { previous: TData[] }>,
+  'mutationFn'
 >
 
 let dummyID = -1
 export function usePost<Spec extends z.ZodTypeAny>(
   spec: Spec,
-  mutationFn: MutationFunction<z.infer<Spec>, Omit<z.infer<Spec>, 'id'>>,
+  invalidateKey: QueryKey,
+  mutationFn: MutationFunction<z.infer<Spec>, PostMutationValue<z.infer<Spec>>>,
   options?: Options<z.infer<Spec>, Omit<z.infer<Spec>, 'id'>>,
 ) {
   return useSafeMutation(spec, mutationFn, {
@@ -19,50 +29,58 @@ export function usePost<Spec extends z.ZodTypeAny>(
       ...old,
       { id: --dummyID, ...(newValue as any) },
     ],
+
+    invalidateKey,
     ...options,
   })
 }
 
 export function usePatch<Spec extends z.ZodTypeAny>(
   spec: Spec,
-  invalidateQuery: QueryKey,
-  mutationFn: MutationFunction<z.infer<Spec>, Partial<z.infer<Spec>>>,
-  options?: Options<z.infer<Spec>, Partial<z.infer<Spec>>>,
+  invalidateKey: QueryKey,
+  mutationFn: MutationFunction<
+    z.infer<Spec>,
+    PatchMutationValue<z.infer<Spec>>
+  >,
+  options?: Options<z.infer<Spec>, PatchMutationValue<z.infer<Spec>>>,
 ) {
   return useSafeMutation(spec, mutationFn, {
-    invalidateQuery,
     setData: (old, newValue) =>
       old.map(t => (t.id === newValue.id ? { ...t, ...newValue } : t)),
+
+    invalidateKey,
     ...options,
   })
 }
 
 export function usePut<Spec extends z.ZodTypeAny>(
   spec: Spec,
-  invalidateQuery: QueryKey,
+  invalidateKey: QueryKey,
   mutationFn: MutationFunction<z.infer<Spec>, z.infer<Spec>>,
   options?: Options<z.infer<Spec>, z.infer<Spec>>,
 ) {
   return useSafeMutation(spec, mutationFn, {
-    invalidateQuery,
     setData: (old, newValue) =>
       old.map(t => (t.id === newValue.id ? newValue : t)),
+
+    invalidateKey,
     ...options,
   })
 }
 
 export function useDelete<Spec extends z.ZodTypeAny, TVariables>(
   spec: Spec,
-  invalidateQuery: any[],
-  mutationFn: MutationFunction<TVariables, TVariables>,
+  invalidateKey: any[],
+  mutationFn: MutationFunction<z.infer<Spec>, TVariables>,
   options?: Omit<
     UseOptimisticMutationOptions<TVariables, TVariables, z.infer<Spec>[]>,
     'setData'
   >,
 ) {
   return useSafeMutation(spec, mutationFn, {
-    invalidateQuery,
     setData: (old, id) => old.filter(t => t.id !== id),
+
+    invalidateKey,
     ...options,
   })
 }
