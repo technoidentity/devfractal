e6670ea024070ad755a0698a5d3895ff7953e334
Pervez Iqbal <pervezfunctor@gmail.com>
2023-09-04 22:47:06 +0530

    use safer router hooks: first router example refactor

---
+++ apps/devfractal-demo/src/react-router-example/routes/hooks.ts
@@ -0,0 +1,7 @@
+import { safeLoaderData, safeParams } from 'devfractal'
+import { number, z } from 'zod'
+import { Contact, ContactList } from '../types'
+
+export const useContact = safeLoaderData(Contact)
+export const useContactList = safeLoaderData(ContactList)
+export const useIdParams = safeParams(z.object({ id: number() }))
