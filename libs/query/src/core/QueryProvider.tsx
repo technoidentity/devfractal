bdac760126bad5156c9118c113e7dddfaa4ab7c3
Pervez Iqbal <pervezfunctor@gmail.com>
2023-09-04 20:18:34 +0530

    DO NOT use query devtools in lib

--- libs/query/src/core/QueryProvider.tsx
+++ libs/query/src/core/QueryProvider.tsx
@@ -1,5 +1,4 @@
 import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 
 import { createQueryClient } from './queryClient'
@@ -31,11 +30,6 @@ export const QueryProvider = ({
   )
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      {process.env.NODE_ENV === 'development' && (
-        <ReactQueryDevtools initialIsOpen={false} />
-      )}
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
 }
