ee179f001d63ee9d0af456ad73f90d72082c3921
Pervez Iqbal <pervezfunctor@gmail.com>
2023-09-06 02:52:21 +0530

    refactor web library

--- apps/typescript-examples/src/asyncExamples.ts
+++ apps/typescript-examples/src/asyncExamples.ts
@@ -1,7 +1,8 @@
 import { Natural, checked } from '@srtp/core'
-import { http } from '@srtp/web'
+import { shttp } from '@srtp/web'
 import invariant from 'tiny-invariant'
 import { z } from 'zod'
+
 export const delay = checked([Natural], ms => {
   invariant(ms >= 0, 'ms must be greater than or equal to 0')
 
@@ -17,7 +18,7 @@ export const failureDelay = <T>(ms: number, message: string): Promise<T> =>
   })
 
 export const timedFetch = <T>(ms: number, url: string): Promise<T> =>
-  Promise.race([failureDelay(ms, 'timed out'), http.get$(url)]) as Promise<T>
+  Promise.race([failureDelay(ms, 'timed out'), shttp.get$(url)]) as Promise<T>
 
 const Post = z.object({
   userId: z.number(),
@@ -31,13 +32,13 @@ const Posts = z.array(Post)
 type Posts = Readonly<z.infer<typeof Posts>>
 
 export const getPosts = () =>
-  http.get(Posts, 'https://jsonplaceholder.typicode.com/posts')
+  shttp.get(Posts, 'https://jsonplaceholder.typicode.com/posts')
 
 export const getPost = (id: number) =>
-  http.get(Post, `https://jsonplaceholder.typicode.com/posts/${id}`)
+  shttp.get(Post, `https://jsonplaceholder.typicode.com/posts/${id}`)
 
 export const getComments = (userId: number) =>
-  http.get(
+  shttp.get(
     Comments,
     `https://jsonplaceholder.typicode.com/posts/${userId}/comments`,
   )
@@ -46,16 +47,16 @@ export const getPostsIds = (...ids: number[]): Promise<Posts> =>
   Promise.all(ids.map(getPost))
 
 export const createPost = (post: Omit<Post, 'id'>) =>
-  http.post(Post, 'https://jsonplaceholder.typicode.com/posts', post)
+  shttp.post(Post, 'https://jsonplaceholder.typicode.com/posts', post)
 
 export const updatePost = (id: number, post: Partial<Post>) =>
-  http.patch(Post, `https://jsonplaceholder.typicode.com/posts/${id}`, post)
+  shttp.patch(Post, `https://jsonplaceholder.typicode.com/posts/${id}`, post)
 
 export const replacePost = (id: number, post: Post) =>
-  http.put(Post, `https://jsonplaceholder.typicode.com/posts/${id}`, post)
+  shttp.put(Post, `https://jsonplaceholder.typicode.com/posts/${id}`, post)
 
 export const deletePost = async (id: number): Promise<void> => {
-  await http.del$(`https://jsonplaceholder.typicode.com/posts/${id}`)
+  await shttp.del$(`https://jsonplaceholder.typicode.com/posts/${id}`)
 }
 
 const Comment = z.object({
@@ -72,14 +73,14 @@ type Comments = Readonly<z.infer<typeof Comments>>
 export const getCommentsByUserId = async (
   userId: number,
 ): Promise<Comments> => {
-  const userPosts = await http.get(
+  const userPosts = await shttp.get(
     Posts,
     `https://jsonplaceholder.typicode.com/users/${userId}/posts`,
   )
 
   const comments = await Promise.all(
     userPosts.map(post =>
-      http.get(
+      shttp.get(
         Comments,
         `https://jsonplaceholder.typicode.com/posts/${post.id}/comments`,
       ),
