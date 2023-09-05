9ec7bbb020575e023efe37d571ce37fcf7fce19a
Pervez Iqbal <pervezfunctor@gmail.com>
2023-09-06 02:53:47 +0530

    fixes to e2e

--- e2e/tests/finance.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from '@playwright/test'
-
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/ctc')
-
-  // Expect a title "to contain" a substring.
-})
