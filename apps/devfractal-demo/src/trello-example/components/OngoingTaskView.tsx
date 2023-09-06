3dd0f7cb79413c3e70b9ca2b1307a450676ef041
Shibashis <shibashis.deb@technoidentity.com>
2023-09-06 21:55:03 +0530

    add trello example

---
+++ apps/devfractal-demo/src/trello-example/components/OngoingTaskView.tsx
@@ -0,0 +1,46 @@
+import { useAtom } from 'jotai'
+import { draggedTaskAtom } from '../state'
+
+type OngoingTaskProps = {
+  ongoingList: string[]
+  onDrop: (task: string) => void
+}
+
+export function OngoingTaskView({
+  ongoingList,
+  onDrop,
+}: OngoingTaskProps): JSX.Element {
+  const [draggedTask, setDraggedTask] = useAtom(draggedTaskAtom)
+
+  return (
+    <main
+      className="flex h-[98vh] w-full flex-col text-black"
+      onDragOver={e => e.preventDefault()}
+      onDrop={() => {
+        onDrop(draggedTask)
+        setDraggedTask('')
+      }}
+    >
+      <h1 className="rounded-md bg-gray-300 p-4 text-center text-2xl font-bold  text-white">
+        Ongoing
+      </h1>
+
+      <section className="max-h-[90vh] space-y-2 overflow-auto bg-white p-2">
+        {ongoingList.length > 0 ? (
+          ongoingList.map(task => (
+            <p
+              key={task}
+              className="cursor-move break-words rounded-xl border-2 px-4 py-2 shadow-md"
+              draggable
+              onDragStart={() => setDraggedTask(task)}
+            >
+              {task}
+            </p>
+          ))
+        ) : (
+          <p className="text-center ">Nothing yet...</p>
+        )}
+      </section>
+    </main>
+  )
+}
---
+++ apps/devfractal-demo/src/trello-example/components/TodoTaskView.tsx
@@ -0,0 +1,71 @@
+import { state } from 'devfractal'
+import { useAtom } from 'jotai'
+import { draggedTaskAtom } from '../state'
+
+type TodoTaskProps = {
+  todoList: string[]
+  onAddTask: (task: string) => void
+}
+
+const useInput = state(
+  { task: '' },
+  {
+    addTask(draft, text: string) {
+      draft.task = text
+    },
+  },
+)
+
+export function TodoTaskView({
+  todoList,
+  onAddTask,
+}: TodoTaskProps): JSX.Element {
+  const [_, setDraggedTask] = useAtom(draggedTaskAtom)
+  const [input, actions] = useInput()
+
+  return (
+    <main className="flex w-full flex-col text-black">
+      <h1 className="rounded-md bg-gray-300 p-4 text-center text-2xl font-bold text-white">
+        Todo
+      </h1>
+      <section className="space-y-2 overflow-auto bg-white p-2">
+        {todoList.length > 0 ? (
+          todoList.map(task => (
+            <p
+              key={task}
+              className="cursor-move break-words rounded-xl border-2 px-4 py-2 shadow-md"
+              draggable
+              onDragStart={() => setDraggedTask(task)}
+            >
+              {task}
+            </p>
+          ))
+        ) : (
+          <p className="text-center">Nothing yet...</p>
+        )}
+      </section>
+
+      <section className="flex justify-between gap-x-2 bg-gray-300 px-2 py-4">
+        <input
+          type="text"
+          name="new-task"
+          id="task"
+          onChange={e => actions.addTask(e.target.value)}
+          value={input.task}
+          placeholder="Add Task"
+          autoFocus
+          className="w-[80%] rounded-xl border-2 px-2"
+        />
+        <button
+          className=" rounded-xl border bg-white px-4 py-1 text-center font-bold"
+          onClick={() => {
+            onAddTask(input.task)
+            actions.addTask('')
+          }}
+        >
+          Add
+        </button>
+      </section>
+    </main>
+  )
+}
