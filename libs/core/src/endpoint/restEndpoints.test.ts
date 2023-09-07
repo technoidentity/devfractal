270148beef1514447c6bec3203bb48ca846e565f
Shibashis <shibashis.deb@technoidentity.com>
2023-09-07 16:17:53 +0530

    add tests

--- libs/core/src/endpoint/restEndpoints.test.ts
+++ libs/core/src/endpoint/restEndpoints.test.ts
@@ -4,18 +4,19 @@ import { restEndpoints } from './restEndpoints'
 
 type RestEndpointsReturnType = ReturnType<typeof restEndpoints>
 
+const spec = {
+  name: z.string(),
+  phone: z.number(),
+  active: z.boolean(),
+  id: z.number().default(100),
+}
+
+const search = {
+  name: z.string(),
+}
+
 describe('restEndpoints', () => {
   test('arguments of type ZodRawShape', () => {
-    const spec = {
-      name: z.string(),
-      phone: z.number(),
-      id: z.number().default(100),
-    }
-
-    const search = {
-      phone: z.number(),
-    }
-
     const expected = {
       many: {
         path: ['/api/test'],
@@ -65,5 +66,61 @@ describe('restEndpoints', () => {
     expect(restEndpoints(spec, search, '/api/test')).toHaveProperty('remove')
 
     expect(restEndpoints(spec, search, '/api/test')).toBeTypeOf(typeof expected)
+  })
+
+  test('argument of type AnyZodObject', () => {
+    const searchObj = z.object(search)
+
+    const expected = {
+      many: {
+        path: ['/api/testing'],
+        method: 'get',
+        response: z.array(z.object(spec)),
+        request: search,
+      },
+
+      one: {
+        path: ['/api/testing', { id: z.number() }],
+        method: 'get',
+        response: z.object(spec),
+        request: z.undefined().optional(),
+      },
+
+      add: {
+        path: ['/api/testing'],
+        method: 'post',
+        response: z.object(spec),
+        request: z.object(spec).omit({ id: true }),
+      },
+
+      update: {
+        path: ['/api/testing', { id: spec['id'] }],
+        method: 'put',
+        response: z.object(spec),
+        request: z.object(spec).partial(),
+      },
+
+      remove: {
+        path: ['/api/testing', { id: z.number() }],
+        method: 'delete',
+        request: z.undefined(),
+        response: z.undefined(),
+      },
+    } as const
+
+    expectTypeOf(restEndpoints(spec, searchObj, '/api/testing')).toMatchTypeOf<
+      Readonly<RestEndpointsReturnType>
+    >()
+
+    expectTypeOf(restEndpoints(spec, search, '/api/testing')).toBeObject()
+    expect(restEndpoints(spec, search, '/api/testing')).toHaveProperty('many')
+    expect(restEndpoints(spec, search, '/api/testing')).toHaveProperty('one')
+    expect(restEndpoints(spec, search, '/api/testing')).toHaveProperty('add')
+    expect(restEndpoints(spec, search, '/api/testing')).toHaveProperty('update')
+    expect(restEndpoints(spec, search, '/api/testing')).toHaveProperty('remove')
+
+    expect(restEndpoints(spec, search, '/api/testing')).toBeTypeOf(
+      typeof expected,
+    )
   })
 })
