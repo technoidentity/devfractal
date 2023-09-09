972f216143b241b4781e6ea75a892ee017cb382a
Pervez Iqbal <pervezfunctor@gmail.com>
2023-09-09 14:58:59 +0530

    minor change to Flex components

--- libs/ui/src/cui/Flex.tsx
+++ libs/ui/src/cui/Flex.tsx
@@ -2,59 +2,37 @@ import React from 'react'
 
 import { cn } from '../utils'
 
-export type FlexDirection = 'row' | 'col' | 'row-reverse' | 'col-reverse'
-
-const flexDirectionClassNames: { [key in FlexDirection]: string } = {
-  row: 'flex-row',
-  col: 'flex-col',
-  'row-reverse': 'flex-row-reverse',
-  'col-reverse': 'flex-col-reverse',
-}
-
 export interface FlexProps extends React.HTMLAttributes<HTMLDivElement> {
-  direction?: FlexDirection
   children: React.ReactNode
 }
 
 export const Flex = React.forwardRef<HTMLDivElement, FlexProps>(
   (props, ref) => {
-    const {
-      direction: flexDirection = 'row',
-      children,
-      className,
-      ...other
-    } = props
+    const { className, ...other } = props
 
-    return (
-      <div
-        ref={ref}
-        className={cn(
-          'flex w-full',
-          flexDirectionClassNames[flexDirection],
-          className,
-        )}
-        {...other}
-      >
-        {children}
-      </div>
-    )
+    return <div ref={ref} className={cn('flex', className)} {...other} />
   },
 )
-
 Flex.displayName = 'Flex'
 
 export const HStack = React.forwardRef<HTMLDivElement, FlexProps>(
   (props, ref) => {
     const { className, ...other } = props
 
-    return <Flex ref={ref} direction="row" className={className} {...other} />
+    return (
+      <div ref={ref} className={cn('flex flex-row', className)} {...other} />
+    )
   },
 )
+HStack.displayName = 'HStack'
 
 export const VStack = React.forwardRef<HTMLDivElement, FlexProps>(
   (props, ref) => {
     const { className, ...other } = props
 
-    return <Flex ref={ref} direction="col" className={className} {...other} />
+    return (
+      <div ref={ref} className={cn('flex flex-col', className)} {...other} />
+    )
   },
 )
+VStack.displayName = 'VStack'
