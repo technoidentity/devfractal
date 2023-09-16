a6adfdde9744026252cd197afba1c85b5b423433
Pervez Iqbal <pervezfunctor@gmail.com>
2023-09-16 18:37:51 +0530

    run type tests too

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -4,5 +4,7 @@ export default defineWorkspace([
   'packages/*/vitest.config.ts',
   'apps/*/vitest.config.ts',
   'libs/*/vitest.config.ts',
+  'experimental/*/vitest.config.ts',
+  'examples/*/vitest.config.ts',
   'starters/*/vitest.config.ts',
 ])
