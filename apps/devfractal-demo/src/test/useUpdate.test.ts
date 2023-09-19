655ebfed3722febc1faee0b8a8362b50d9d4475e
Shibashis <shibashis.deb@technoidentity.com>
2023-09-19 17:02:58 +0530

    add useUpdate hook test

---
+++ apps/devfractal-demo/src/test/useUpdate.test.ts
@@ -0,0 +1,42 @@
+import { act, renderHook } from '@testing-library/react'
+import { useUpdate, type UpdateHandlers } from 'devfractal'
+import { expect, expectTypeOf, test } from 'vitest'
+
+test('useUpdate', () => {
+  const initialState = { count: 0, step: 1 }
+
+  const { result } = renderHook(() => useUpdate(initialState))
+
+  expectTypeOf(result.current[0]).toMatchTypeOf<typeof initialState>()
+  expectTypeOf(result.current[1]).toMatchTypeOf<
+    UpdateHandlers<typeof initialState>
+  >()
+
+  act(() => {
+    result.current[1].updateCount((count: number) => count + 1)
+  })
+  expect(result.current[0]).toEqual({ count: 1, step: 1 })
+
+  act(() => {
+    result.current[1].updateStep((step: number) => step * 2)
+  })
+  expect(result.current[0]).toEqual({ count: 1, step: 2 })
+
+  act(() => {
+    result.current[1].setCount(3)
+  })
+  expect(result.current[0]).toEqual({ count: 3, step: 2 })
+
+  act(() => {
+    result.current[1].setStep(4)
+  })
+  expect(result.current[0]).toEqual({ count: 3, step: 4 })
+
+  act(() => {
+    result.current[1].update((state: typeof initialState) => ({
+      count: state.count + 2,
+      step: state.step - 1,
+    }))
+  })
+  expect(result.current[0]).toEqual({ count: 5, step: 3 })
+})
