ac553e19afef74f1a263e679e1b144254c6d5186
Pervez Iqbal <pervezfunctor@gmail.com>
2023-09-21 01:38:59 +0530

    add msw configuration for router-examples

--- examples/router-examples/src/main.tsx
+++ examples/router-examples/src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from '@srtp/react'
 import { ContactsApp } from '@/contacts'
 import './global.css'
 
+if (import.meta.env.DEV) {
+  const { worker } = await import('./mocks/browser')
+  await worker.start({ onUnhandledRequest: 'bypass' })
+}
+
 const root = createRoot('root')
 
 root.render(<ContactsApp />)
