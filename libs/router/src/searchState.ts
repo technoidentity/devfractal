b37618c28a489f632d3fd69e65ed23010838a333
Pervez Iqbal <pervezfunctor@gmail.com>
2023-10-01 03:04:53 +0530

    searchState should provide links too WIP

--- libs/router/src/searchState.ts
+++ libs/router/src/searchState.ts
@@ -1,16 +1,19 @@
-import type { Tail } from '@srtp/core'
+import { cast, type Tail } from '@srtp/core'
 import { omap$ } from '@srtp/fn'
 import type { Handlers } from '@srtp/react'
+import { toSearchParams } from '@srtp/web'
+import { produce } from 'immer'
 import React from 'react'
 import type { z } from 'zod'
 
 import { safeSearch } from './safeHooks'
 
-export type SearchActions<
-  Spec extends z.ZodTypeAny,
-  Hs extends Handlers<z.infer<Spec>>,
-> = {
+export type SearchStateActions<Hs extends Handlers<any>> = {
   [A in keyof Hs]: (...args: Tail<Parameters<Hs[A]>>) => void
+}
+
+export type SearchStateSearchParams<Hs extends Handlers<any>> = {
+  [A in keyof Hs]: (...args: Tail<Parameters<Hs[A]>>) => URLSearchParams
 }
 
 // @TODO: support initialValue?
@@ -22,7 +25,8 @@ export function searchState<
 
   return function useSearchState(): readonly [
     z.infer<Spec>,
-    SearchActions<Spec, Hs>,
+    SearchStateActions<Hs>,
+    SearchStateSearchParams<Hs>,
   ] {
     const [search, setSearch] = useSearch()
 
@@ -36,6 +40,19 @@ export function searchState<
       [setSearch],
     )
 
-    return [search, actions] as const
+    const searchParams = React.useMemo(
+      () =>
+        omap$(handlers, fn => (...args: any[]) => {
+          const search = cast(spec, args[0])
+          const newState = produce(search, (draft: any) => {
+            fn(draft, ...args)
+          })
+
+          return toSearchParams(newState)
+        }),
+      [],
+    )
+
+    return [search, actions, searchParams] as const
   }
 }
