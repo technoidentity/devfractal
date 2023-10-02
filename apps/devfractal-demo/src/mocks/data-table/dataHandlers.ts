78cbe3268f48ccd483343dd1cca73d10b0e991ce
Shibashis <shibashis.deb@technoidentity.com>
2023-10-02 20:27:48 +0530

    Add query for page and limit; add pagination fns

--- apps/devfractal-demo/src/mocks/data-table/dataHandlers.ts
+++ apps/devfractal-demo/src/mocks/data-table/dataHandlers.ts
@@ -1,8 +1,16 @@
+import { isNotNull, toInt } from 'devfractal'
 import { rest } from 'msw'
-import { products } from '@/server-side/products'
+import { getProducts } from './operations'
 
 export const dataHandlers = [
-  rest.get('/api/data/products', (_req, res, ctx) => {
-    return res(ctx.json(products))
+  rest.get('/api/data/products', (req, res, ctx) => {
+    const page = isNotNull(req.url.searchParams.get('page'))
+      ? toInt(req.url.searchParams.get('page'))
+      : 1
+    const limit = isNotNull(req.url.searchParams.get('limit'))
+      ? toInt(req.url.searchParams.get('limit'))
+      : 10
+
+    return res(ctx.json(getProducts(page, limit)))
   }),
 ]
