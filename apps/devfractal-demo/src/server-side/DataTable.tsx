f110a1c4f2ff71618d769a990bcb6a06af2a23cf
Shibashis <shibashis.deb@technoidentity.com>
2023-10-02 20:27:48 +0530

    Add query for page and limit; add pagination fns

--- apps/devfractal-demo/src/server-side/DataTable.tsx
+++ apps/devfractal-demo/src/server-side/DataTable.tsx
@@ -1,21 +1,63 @@
 import { useQuery } from '@tanstack/react-query'
-import { Table, VStack } from 'devfractal'
+import { Table, VStack, Text, HStack, toInt } from 'devfractal'
+import React from 'react'
 import { DataBody } from './components/DataBody'
 import { DataHeader } from './components/DataHeaders'
 import { Pagination } from './components/Pagination'
 import { fetchProducts } from './query'
 
 export function DataTable(): JSX.Element {
-  const result = useQuery({ queryKey: ['products'], queryFn: fetchProducts })
+  const [state, setState] = React.useState({ page: 1, limit: 10 }) // URL state
+  const { isLoading, isSuccess, data } = useQuery({
+    queryKey: ['products', state.page, state.limit],
+    queryFn: () => fetchProducts(state.page, state.limit),
+  })
+
+  function handleLimit(value: string) {
+    setState(state => ({ ...state, limit: toInt(value) }))
+  }
+
+  function handleNext() {
+    setState(state => ({ ...state, page: state.page + 1 }))
+  }
+
+  function handlePrev() {
+    setState(state => ({ ...state, page: state.page - 1 }))
+  }
+
+  function handleFirst() {
+    setState(state => ({ ...state, page: 1 }))
+  }
+
+  function handleLast(last: number) {
+    setState(state => ({ ...state, page: last }))
+  }
 
   return (
     <VStack className="justify-center items-center h-screen overflow-y-auto p-2 border rounded-md">
-      <Table className="text-center">
-        <DataHeader />
-        <DataBody data={result.data!} />
-      </Table>
+      {isLoading ? (
+        <HStack>Loading....</HStack>
+      ) : isSuccess ? (
+        <>
+          <Table className="text-center">
+            <DataHeader />
+            <DataBody data={data.products} />
+          </Table>
 
-      <Pagination />
+          <Pagination
+            currentPage={state.page}
+            limit={state.limit}
+            totalPages={data.totalPages}
+            onFirst={handleFirst}
+            onLast={handleLast}
+            onNext={handleNext}
+            onPrev={handlePrev}
+            onSetLimit={handleLimit}
+          />
+        </>
+      ) : (
+        <Text>No data found!</Text>
+      )}
     </VStack>
   )
 }
