245604c95d119c9da9a356e7732a00fcef64ed5a
Shibashis <shibashis.deb@technoidentity.com>
2023-10-09 16:03:16 +0530

    add ordering in server-side table

--- apps/devfractal-demo/src/server-side/query.ts
+++ apps/devfractal-demo/src/server-side/query.ts
@@ -1,8 +1,15 @@
 import type { ProductsResponse } from '@/mocks/data-table/operations'
-
-export const fetchProducts = async (page: number, limit: number) => {
+// @TODO: make fn to append query parameters to base url
+export const fetchProducts = async (
+  page: number,
+  limit: number,
+  key: string,
+  order: 'asc' | 'desc',
+) => {
   const data: ProductsResponse = await (
-    await fetch(`/api/data/products/?page=${page}&limit=${limit}`)
+    await fetch(
+      `/api/data/products/?page=${page}&limit=${limit}&key=${key}&order=${order}`,
+    )
   ).json()
 
   return data
