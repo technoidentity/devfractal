f7255d8f4f0e9dd6b62a0b0936ced7bf56bdbfb2
Shibashis <shibashis.deb@technoidentity.com>
2023-10-24 18:26:06 +0530

    minor changes

--- apps/devfractal-demo/src/server-side/products.ts
+++ apps/devfractal-demo/src/server-side/products.ts
@@ -2,10 +2,6 @@
 
 export type Products = typeof data
 export type Product = (typeof data)[number]
-export type ProductWithoutTitle = Omit<Product, 'title'>
-export type ProductWithoutPrice = Omit<Product, 'price'>
-export type ProductWithoutBrand = Omit<Product, 'brand'>
-export type ProductWithoutCategory = Omit<Product, 'category'>
 
 export const headers = ['title', 'price', 'brand', 'category']
 
